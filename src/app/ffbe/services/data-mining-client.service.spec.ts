2948c9f2bb8e7e7788e10b7ad39ccfb4a7ba1f1d
Mingard Patrick ( U113286 ) <patrick.mingard@mobi.ch>
2018-02-12 13:28:40 +0100

    Unit testing data mining client service.

--- src/app/ffbe/services/data-mining-client.service.spec.ts
+++ src/app/ffbe/services/data-mining-client.service.spec.ts
@@ -1,15 +1,38 @@
-import {TestBed, inject} from '@angular/core/testing';
-
+import {inject, TestBed} from '@angular/core/testing';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 import {DataMiningClientService} from './data-mining-client.service';
+import {HttpClient} from '@angular/common/http';
 
-xdescribe('DataMiningClientService', () => {
+class HttpClientMock {
+  public get(url: string): Observable<Object> {
+    return Observable.of(null);
+  }
+}
+
+describe('DataMiningClientService', () => {
+  let httpClient: HttpClient = null;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataMiningClientService]
+      providers: [DataMiningClientService,
+        {provide: HttpClient, useClass: HttpClientMock}]
     });
   });
 
+  beforeEach(inject([HttpClient], (client: HttpClient) => {
+    httpClient = client;
+    spyOn(httpClient, 'get').and.callThrough();
+  }));
+
   it('should be created', inject([DataMiningClientService], (service: DataMiningClientService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should delegate to HttpClient', inject([DataMiningClientService], (service: DataMiningClientService) => {
+    // WHEN
+    service.getCharacters$();
+    // THEN
+    expect(httpClient.get).toHaveBeenCalled();
   }));
 });
