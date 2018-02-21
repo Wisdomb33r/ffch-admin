ae9cc90b467b492c55e87ef27f8f1ef15ecb5575
Wisdomb33r <info@final-fantasy.ch>
2018-02-21 13:19:37 +0100

    Adding a converter to determine the category of a skill based on the multiple attributes the the skill model. Displaying the skill type in the skill list.

--- src/app/ffbe/mappers/skill-mapper.ts
+++ src/app/ffbe/mappers/skill-mapper.ts
@@ -7,7 +7,7 @@ export class SkillMapper {
   public static toCompetence(skill: Skill): Competence {
     return new Competence(
       skill.gumi_id,
-      1, // TODO algorithm to map the Skill to a Competence categorie according to the FFCH identifiers
+      SkillMapper.determineCategorieCompetence(skill),
       skill.strings.name[FFBE_FRENCH_TABLE_INDEX],
       skill.strings.name[FFBE_ENGLISH_TABLE_INDEX],
       skill.strings.desc_short[FFBE_FRENCH_TABLE_INDEX],
@@ -18,5 +18,38 @@ export class SkillMapper {
       skill.attack_count.length > 0 ? skill.attack_count[0] : null,
       skill.attack_frames.length > 0 ? skill.attack_frames[0].join(" ") : null
     );
+  }
+
+  private static determineCategorieCompetence(skill: Skill) {
+    if (skill.type == 'MAGIC') {
+      if (skill.magic_type === 'White') {
+        return 1;
+      }
+      if (skill.magic_type === 'Black') {
+        return 2;
+      }
+      if (skill.magic_type === 'Green') {
+        return 3;
+      }
+    }
+    if (skill.type === 'ABILITY') {
+      if (!skill.active) {
+        return 4;
+      }
+      if (Array.isArray(skill.attack_count) && skill.attack_count.length > 0 && skill.attack_count[0] > 0) {
+        if (skill.attack_type === 'Physical') {
+          return 6;
+        }
+        if (skill.attack_type === 'Magic') {
+          return 7;
+        }
+        if (skill.attack_type === 'Hybrid') {
+          return 8;
+        }
+        return 5;
+      }
+      return 5;
+    }
+    return undefined;
   }
 }
--- src/app/ffbe/model/skill.model.ts
+++ src/app/ffbe/model/skill.model.ts
@@ -4,10 +4,12 @@ export class Skill {
   public gumi_id: number;
   public name: string;
   public type: string;
+  public active: boolean;
   public magic_type: string;
   public mp_cost: number;
   public attack_count: Array<number>;
   public attack_frames: Array<Array<number>>;
+  public attack_type: string;
   public effects: Array<string>;
   public strings: SkillStrings;
 }
