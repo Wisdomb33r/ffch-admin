5359cdce72c2c635b57e6141d07e60a4738e9509
Sylvie Stucki <Sylvie@users.noreply.github.com>
2018-05-19 19:02:27 +0200

    Added a method "produce" in UniteMateriauEveil and UniteMateriauxEveil
    This static method creates a new UniteMateriau(x)Eveil on the basis of the one given in argument.
    This allows creating new objects from "any" items.

--- src/app/ffbe/model/unite-materiau-eveil.model.ts
+++ src/app/ffbe/model/unite-materiau-eveil.model.ts
@@ -8,6 +8,13 @@ export class UniteMateriauEveil {
               public quantite: number) {
   }
 
+  public static produce(m: UniteMateriauEveil)
+  {
+    const  materiauEveil = new UniteMateriauEveil(m.gumi_id, m.quantite);
+    materiauEveil.materiau = Objet.produce(m.materiau);
+    return materiauEveil;
+  }
+
   public isPresentInFfchDb(): boolean {
     return !isNullOrUndefined(this.materiau) && this.materiau.isPresentInFfchDb();
   }
--- src/app/ffbe/model/unite-materiaux-eveil.model.ts
+++ src/app/ffbe/model/unite-materiaux-eveil.model.ts
@@ -1,6 +1,16 @@
 import {UniteMateriauEveil} from "./unite-materiau-eveil.model";
 
 export class UniteMateriauxEveil {
-  public unite_numero : number;
-  public materiaux : Array<UniteMateriauEveil>;
+
+  constructor(  public unite_numero : number,
+  public materiaux : Array<UniteMateriauEveil>)
+  {
+  }
+
+  public static produce(ume: UniteMateriauxEveil) : UniteMateriauxEveil
+  {
+    const materiaux = ume.materiaux.map(materiau => UniteMateriauEveil.produce(materiau));
+    return new UniteMateriauxEveil(ume.unite_numero, materiaux);
+  }
+
 }
