96ef70bd83ad688d5940e6c90d7f65accd5ad4c4
Sylvie Stucki <Sylvie@users.noreply.github.com>
2018-08-12 11:57:02 +0200

    Added skeleton of the Enhancements-Display component
    The component is loaded by the Enhancement component.

---
+++ src/app/ffbe/enhancements-display/enhancements-display.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Amelioration} from '../model/amelioration.model';
+
+@Component({
+  selector: 'app-enhancements-display',
+  templateUrl: './enhancements-display.component.html',
+  styleUrls: ['./enhancements-display.component.css']
+})
+export class EnhancementsDisplayComponent implements OnInit {
+
+  @Input() ameliorations: Array<Amelioration>;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
