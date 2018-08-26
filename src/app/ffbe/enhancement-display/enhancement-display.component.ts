7c325da8dfd75007316b3da2a1ca49bbab9bfbe5
Sylvie Stucki <Sylvie@users.noreply.github.com>
2018-08-26 09:53:07 +0200

    Added display of competences
    The EnhancementDisplay components includes a CharacterSkillsDisplay component for this task.
    
    For enhancements of type "+1", the base skill and the new skill are diplayed.
    For type "+2", the base skill, old skill and new skill are displayed.
    The order is not defined, it depends on the order of the responses coming from the backend.

--- src/app/ffbe/enhancement-display/enhancement-display.component.ts
+++ src/app/ffbe/enhancement-display/enhancement-display.component.ts
@@ -6,6 +6,10 @@ import {forEach} from '@angular/router/src/utils/collection';
 import {Character} from '../model/character.model';
 import {CharacterMapper} from '../mappers/character-mapper';
 import {Personnage} from '../model/personnage.model';
+import {Competence} from '../model/competence.model';
+import {SkillsService} from '../services/skills.service';
+import {isNullOrUndefined} from 'util';
+import {SkillMapper} from '../mappers/skill-mapper';
 
 @Component({
   selector: 'app-enhancement-display',
@@ -17,35 +21,51 @@ export class EnhancementDisplayComponent implements OnInit, OnChanges {
   @Input() amelioration: Amelioration;
   public displayed = false;
   public personnages: Array<Personnage>;
+  public competences: Array<Competence>;
 
-  constructor(private charactersService: CharactersService) { }
+  constructor(private charactersService: CharactersService,
+              private  skillService: SkillsService) {
+  }
 
   ngOnInit() {
   }
 
   ngOnChanges() {
     this.getPersonnages();
+    this.getCompetences();
   }
 
   public switchDisplayed() {
     this.displayed = !this.displayed;
   }
 
-  public isSingleCharacter()
-  {
+  public isSingleCharacter() {
     return this.personnages.length === 1;
   }
 
-  protected getPersonnages()
-  {
+  protected getPersonnages() {
     this.personnages = [];
-    this.amelioration.units.forEach(unit => this.personnages.push(CharacterMapper.toPersonnage(this.charactersService.searchForCharacterByGumiId(unit))));
-    if (this.personnages.length == 1)
-    {
+    this.amelioration.units.forEach(unit =>
+      this.personnages.push(CharacterMapper.toPersonnage(this.charactersService.searchForCharacterByGumiId(unit))));
+    if (this.personnages.length === 1) {
       this.amelioration.perso_gumi_id = this.personnages[0].gumi_id;
     }
   }
 
+  protected getCompetences() {
+    this.competences = [];
+    if (!isNullOrUndefined(this.amelioration.skill_id_base)) {
+      this.competences.push(SkillMapper.toCompetence(this.skillService.searchForSkillByGumiId(this.amelioration.skill_id_base)));
+    }
+    if (!isNullOrUndefined(this.amelioration.skill_id_old) && (this.amelioration.skill_id_old !== this.amelioration.skill_id_base)) {
+      this.competences.push(SkillMapper.toCompetence(this.skillService.searchForSkillByGumiId(this.amelioration.skill_id_old)));
+    }
+    if (!isNullOrUndefined(this.amelioration.skill_id_new)) {
+      this.competences.push(SkillMapper.toCompetence(this.skillService.searchForSkillByGumiId(this.amelioration.skill_id_new)));
+    }
+  }
 
-
+  public areSkillsDisplayed(): boolean {
+    return Array.isArray(this.competences) && this.competences.length > 0;
+  }
 }
