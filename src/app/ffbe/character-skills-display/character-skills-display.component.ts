dcbb30f3d2ce6104e1558239a28ef26f4f8187bb
Sylvie Stucki <Sylvie@users.noreply.github.com>
2018-09-08 11:05:58 +0200

    Updated the CharacterSkillDisplay component to fetch the competences in the specified order
    The first version used a forEach, which didn't preserve the order of the competences.
    Changed the forEach into a forkJoin to preserve the order.
    The errors are not handled yet.

--- src/app/ffbe/character-skills-display/character-skills-display.component.ts
+++ src/app/ffbe/character-skills-display/character-skills-display.component.ts
@@ -4,6 +4,9 @@ import {FfchClientService} from '../services/ffch-client.service';
 import {isNullOrUndefined} from 'util';
 import {CompetencesComparingContainer} from '../model/competences-comparing-container.model';
 import {SkillMapper} from '../mappers/skill-mapper';
+import {Observable} from 'rxjs/Observable';
+import {forkJoin} from 'rxjs/observable/forkJoin';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   selector: 'app-character-skills-display',
@@ -26,13 +29,17 @@ export class CharacterSkillsDisplayComponent implements OnInit, OnChanges {
     this.skillsErrors = [];
     this.competencesContainers = [];
     if (Array.isArray(this.competences)) {
+      const observables:Array< Observable<Competence> > = [] ;
       this.competences.forEach(competence => {
-        this.ffchClientService.getCompetenceByGumiId$(competence.gumi_id)
-          .subscribe(c => {
-              competence.id = isNullOrUndefined(c) ? undefined : c.id;
-              this.competencesContainers.push(new CompetencesComparingContainer(competence, c));
+        //observables.push( this.ffchClientService.getCompetenceByGumiId$(competence.gumi_id).pipe(catchError(error => this.skillsErrors.push('Erreur lors du traitement de la compétence ' + competence.nom + ' : ' + error))) );
+        observables.push( this.ffchClientService.getCompetenceByGumiId$(competence.gumi_id) );
+      });
+      forkJoin(observables).subscribe(results => {
+      results.forEach( (c, index) => {
+              this.competences[index].id = isNullOrUndefined(c) ? undefined : c.id;
+              this.competencesContainers.push(new CompetencesComparingContainer(this.competences[index], c));
             },
-            error => this.skillsErrors.push('Erreur lors du traitement de la compétence ' + competence.nom + ' : ' + error)
+            //error => this.skillsErrors.push('Erreur lors du traitement de la compétence ' + competence.nom + ' : ' + error)
           );
       });
     }
