8e5f27b4e7b0ddf7b739eae01079fa1c1d4a84e2
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-04-06 18:55:19 +0200

    Added the search form and the method "searchItemRecipeInDataMining" in the ItemRecipeComponent
    The ItemRecipeComponent only displays the ItemRecipe received in the console.
    The class Recette is empty for now.

--- src/app/ffbe/item-recipes/item-recipes.component.ts
+++ src/app/ffbe/item-recipes/item-recipes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {ItemRecipesService} from '../services/item-recipes.service';
+import {Recette} from '../model/recette.model';
+import {ItemRecipe} from '../model/item-recipe.model';
+import {isNullOrUndefined} from 'util';
 
 @Component({
   selector: 'app-item-recipes',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemRecipesComponent implements OnInit {
 
-  constructor() { }
+  englishName: FormControl;
+  frenchName: FormControl;
+  gumiId: FormControl;
+  recettes: Array<Recette>;
+
+  constructor(private itemRecipesService: ItemRecipesService) {
+    this.englishName = new FormControl('');
+    this.frenchName = new FormControl('');
+    this.gumiId = new FormControl('');
+  }
 
   ngOnInit() {
   }
 
+  public searchItemRecipeInDataMining() {
+    this.recettes = [];
+    if (!isNullOrUndefined(this.gumiId.value) && this.gumiId.value > 0) {
+      this.englishName.patchValue('');
+      this.frenchName.patchValue('');
+      const itemRecipe = this.itemRecipesService.searchForItemRecipeByGumiId(this.gumiId.value);
+      if (!isNullOrUndefined((itemRecipe))) {
+        console.log(itemRecipe);
+        //this.competences.push(SkillMapper.toCompetence(skill));
+      }
+    } else {
+      const itemRecipes: Array<ItemRecipe> = this.itemRecipesService.searchForItemRecipesByNames(this.englishName.value, this.frenchName.value);
+      console.log(itemRecipes);
+      //itemRecipes.forEach(skill => this.competences.push(SkillMapper.toCompetence(skill)));
+    }
+  }
+
+  public isItemRecipeDisplayed(): boolean {
+    return Array.isArray(this.recettes) && this.recettes.length > 0;
+  }
+
+  public isDataMiningLoading(): boolean {
+    return !this.itemRecipesService.isLoaded();
+  }
 }
