faa94f33ee018a5a906592e564f935d9862d77c9
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-05-01 14:50:38 +0200

    Displayed the picture of the Objet when available
    The picture is only available for Objets that are found in the DB and that have a picture there.

--- src/app/ffbe/objets-display/objets-display.component.ts
+++ src/app/ffbe/objets-display/objets-display.component.ts
@@ -40,7 +40,7 @@ export class ObjetsDisplayComponent implements OnChanges, OnDestroy {
       this.subscription = forkJoin(observables).subscribe(results => {
         results.forEach((o, index) => {
             this.objets[index].id = isNullOrUndefined(o) ? undefined : o.id;
-            this.objetsContainer.push(new ObjetsComparingContainer(this.objets[index], o));
+          this.objetsContainer.push(new ObjetsComparingContainer(this.objets[index], isNullOrUndefined(o) ? null : Objet.produce(o)));
           }
         );
       });
