ea166c12822aa5b82644f53e5cdbf1eda778ee1d
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-07-06 18:28:36 +0200

    Refactored skills.php
    Moved reusable parts of the POST handler in separate functions.

--- src/php/skills.php
+++ src/php/skills.php
@@ -57,59 +57,10 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
       $errors [] = 'Duplicate key exception : Gumi id ' . $competence->gumi_id . ' already exists';
       echo json_encode($errors);
     } else {
-      $values = array();
-      if (isset ($competence->nom))
-        $values ['nom'] = $competence->nom;
-      if (isset ($competence->nom_en))
-        $values ['nom_en'] = $competence->nom_en;
-      if (isset ($competence->description))
-        $values ['description'] = $competence->description;
-      if (isset ($competence->effet))
-        $values ['effet'] = $competence->effet;
-      if (isset ($competence->puissance))
-        $values ['puissance'] = $competence->puissance;
-      if (isset ($competence->physique))
-        $values ['physique'] = $competence->physique;
-      if (isset ($competence->magique))
-        $values ['magique'] = $competence->magique;
-      if (isset ($competence->hybride))
-        $values ['hybride'] = $competence->hybride;
-      if (isset ($competence->pm))
-        $values ['pm'] = $competence->pm;
-      if (isset ($competence->lb))
-        $values ['lb'] = $competence->lb;
-      if (isset ($competence->ep))
-        $values ['ep'] = $competence->ep;
-      if (isset ($competence->hits))
-        $values ['hits'] = $competence->hits;
-      if (isset ($competence->frames))
-        $values ['frames'] = $competence->frames;
-      if (isset ($competence->damages))
-        $values ['damages'] = $competence->damages;
-      $values ['gumi_id'] = $competence->gumi_id;
-      if (isset ($competence->enhanced))
-        $values ['enhanced'] = ($competence->enhanced == true) ? '1' : '0';
+      $values = createPropertyArray($competence);
       $brex_competence = new brex_competence ($values);
-      if (isset ($competence->categorie) && ($categorie = brex_compet_categ::findByPrimaryId($competence->categorie)))
-        $brex_competence->setrelationcategorie($categorie);
-      if (isset ($competence->icone) && ($icone = brex_compet_image::findByPrimaryId($competence->icone)))
-        $brex_competence->setrelationicone($icone);
-      if ($brex_competence->store()) {
-        $brex_competences = brex_competence::finderParGumiId($competence->gumi_id);
-        if (count($brex_competences) > 0) {
-          http_response_code(201);
-          $competence = new Competence ($brex_competences [0]);
-          echo json_encode($competence, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
-        } else {
-          http_response_code(500);
-          $errors = array();
-          $errors [] = 'Unexpected error occured, stored gumi id ' . $competence->gumi_id . ' not found';
-          echo json_encode($errors);
-        }
-      } else {
-        http_response_code(400);
-        echo json_encode($brex_competence->errors);
-      }
+      updateRelations($brex_competence, $competence);
+      storeAndRespond($brex_competence, $competence, 201);
     }
   } else {
     http_response_code(400);
@@ -130,4 +81,71 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
     http_response_code(400);
   }
 }
+
+function createPropertyArray($competence)
+{
+  $values = array();
+  if (isset ($competence->nom))
+    $values ['nom'] = $competence->nom;
+  if (isset ($competence->nom_en))
+    $values ['nom_en'] = $competence->nom_en;
+  if (isset ($competence->description))
+    $values ['description'] = $competence->description;
+  if (isset ($competence->effet))
+    $values ['effet'] = $competence->effet;
+  if (isset ($competence->puissance))
+    $values ['puissance'] = $competence->puissance;
+  if (isset ($competence->physique))
+    $values ['physique'] = $competence->physique;
+  if (isset ($competence->magique))
+    $values ['magique'] = $competence->magique;
+  if (isset ($competence->hybride))
+    $values ['hybride'] = $competence->hybride;
+  if (isset ($competence->pm))
+    $values ['pm'] = $competence->pm;
+  if (isset ($competence->lb))
+    $values ['lb'] = $competence->lb;
+  if (isset ($competence->ep))
+    $values ['ep'] = $competence->ep;
+  if (isset ($competence->hits))
+    $values ['hits'] = $competence->hits;
+  if (isset ($competence->frames))
+    $values ['frames'] = $competence->frames;
+  if (isset ($competence->damages))
+    $values ['damages'] = $competence->damages;
+  $values ['gumi_id'] = $competence->gumi_id;
+  if (isset ($competence->enhanced))
+    $values ['enhanced'] = ($competence->enhanced == true) ? '1' : '0';
+
+  return $values;
+}
+
+function updateRelations($brex_competence, $competence)
+{
+  if (isset ($competence->categorie) && ($categorie = brex_compet_categ::findByPrimaryId($competence->categorie)))
+    $brex_competence->setrelationcategorie($categorie);
+  if (isset ($competence->icone) && ($icone = brex_compet_image::findByPrimaryId($competence->icone)))
+    $brex_competence->setrelationicone($icone);
+}
+
+function storeAndRespond($brex_competence, $competence, $successCode)
+{
+  if ($brex_competence->store()) {
+    $brex_competences = brex_competence::finderParGumiId($competence->gumi_id);
+    if (count($brex_competences) > 0) {
+      http_response_code($successCode);
+      $competence = new Competence ($brex_competences [0]);
+      echo json_encode($competence, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+    } else {
+      http_response_code(500);
+      $errors = array();
+      $errors [] = 'Unexpected error occurred, stored gumi id ' . $competence->gumi_id . ' not found';
+      echo json_encode($errors);
+    }
+  } else {
+    http_response_code(400);
+    echo json_encode($brex_competence->errors);
+  }
+}
+
 ?>
