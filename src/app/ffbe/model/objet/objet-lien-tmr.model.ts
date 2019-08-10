4387d08b9f5a805164a28c8ea8d2f4fb8cd8e0af
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-08-10 10:56:17 +0200

    Fixed typo in ObjetLienTMR
    -

--- src/app/ffbe/model/objet/objet-lien-tmr.model.ts
+++ src/app/ffbe/model/objet/objet-lien-tmr.model.ts
@@ -5,7 +5,7 @@ export class ObjetLienTMR {
   public constructor(
     public perso_gumi_id: number,
     public nom_perso: string,
-    public isSTRM: boolean
+    public isSTMR: boolean
   ) {
   }
 
@@ -13,7 +13,7 @@ export class ObjetLienTMR {
     if (!isNotNullOrUndefined(lien)) {
       return null;
     }
-    return new ObjetLienTMR(lien.perso_gumi_id, lien.nom_perso, lien.isSTRM);
+    return new ObjetLienTMR(lien.perso_gumi_id, lien.nom_perso, lien.isSTMR);
   }
 
 }
