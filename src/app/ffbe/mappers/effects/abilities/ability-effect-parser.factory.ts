06128c48ac6811becd60c12f08cea4cf6f207998
Sylvie <Sylvie@users.noreply.github.com>
2019-09-23 07:32:53 +0200

    Refactored the AbilityEffectParserFactory
    ... to select the parser depending on the 3rd effectId.

--- src/app/ffbe/mappers/effects/abilities/ability-effect-parser.factory.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-effect-parser.factory.ts
@@ -6,35 +6,23 @@ import {AbilityAilmentsInflictionParser} from './ability-ailments-infliction.par
 import {AbilityAilmentsResistanceParser} from './ability-ailments-resistance.parser';
 import {AbilitySkillModifierIncreaseParser} from './ability-skill-modifier-increase.parser';
 
-const PARSERS: Array<{ key: string, parser: EffectParser }> = [
-  {key: '0-3-3', parser: new AbilityStatsModificationParser()},
-  {key: '1-3-3', parser: new AbilityStatsModificationParser()},
-  {key: '1-2-3', parser: new AbilityStatsModificationParser()},
-  {key: '2-2-3', parser: new AbilityStatsModificationParser()},
-  {key: '2-5-3', parser: new AbilityStatsModificationParser()},
-  {key: '1-1-24', parser: new AbilityStatsModificationParser()},
-  {key: '2-1-24', parser: new AbilityStatsModificationParser()},
-  {key: '0-3-33', parser: new AbilityElementResistancesParser()},
-  {key: '2-2-33', parser: new AbilityElementResistancesParser()},
-  {key: '1-2-33', parser: new AbilityElementResistancesParser()},
-  {key: '1-1-33', parser: new AbilityElementResistancesParser()},
-  {key: '2-1-33', parser: new AbilityElementResistancesParser()},
-  {key: '0-3-6', parser: new AbilityAilmentsInflictionParser()},
-  {key: '1-1-6', parser: new AbilityAilmentsInflictionParser()},
-  {key: '2-1-6', parser: new AbilityAilmentsInflictionParser()},
-  {key: '0-3-7', parser: new AbilityAilmentsResistanceParser()},
-  {key: '2-2-7', parser: new AbilityAilmentsResistanceParser()},
-  {key: '0-3-136', parser: new AbilitySkillModifierIncreaseParser()},
-  {key: '1-3-136', parser: new AbilitySkillModifierIncreaseParser()},
-  {key: '1-2-136', parser: new AbilitySkillModifierIncreaseParser()},
-];
-
 export class AbilityEffectParserFactory {
   public static getParser(effectId1: number, effectId2: number, effectId3: number): EffectParser {
-    const parserItem = PARSERS.find(val => val.key === effectId1 + '-' + effectId2 + '-' + effectId3);
-    if (parserItem) {
-      return parserItem.parser;
+    switch (effectId3) {
+      case 3:
+        return new AbilityStatsModificationParser();
+      case 6:
+        return new AbilityAilmentsInflictionParser();
+      case 7:
+        return new AbilityAilmentsResistanceParser();
+      case 24:
+        return new AbilityStatsModificationParser();
+      case 33:
+        return new AbilityElementResistancesParser();
+      case 136:
+        return new AbilitySkillModifierIncreaseParser();
+      default:
+        return new UnknownEffectParser();
     }
-    return new UnknownEffectParser();
   }
 }
