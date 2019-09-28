93486efbe06b7d20ef169af5a19f6da646382008
Sylvie <Sylvie@users.noreply.github.com>
2019-09-28 21:45:43 +0200

    Added two cases in AbilityCooldownParser
    ... and refactored a bit.

--- src/app/ffbe/mappers/effects/abilities/ability-cooldown-parser.spec.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-cooldown-parser.spec.ts
@@ -1,12 +1,30 @@
 import {AbilityEffectParserFactory} from './ability-effect-parser.factory';
 
 fdescribe('AbilityCooldownParser', () => {
-  it('should parse cooldowns with three items and same value in array', () => {
+  it('should parse cooldowns with three items and same values in array', () => {
     // GIVEN
     const effect = JSON.parse('[0, 3, 130, [507362, 1, [2,  2]]]');
     // WHEN
     const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
     // THEN
     expect(s).toEqual('(Une fois tous les 3 tours)<br />507362<br />Disponible dès le tour 1');
+  });
+
+  it('should parse cooldowns with three items and different values in array', () => {
+    // GIVEN
+    const effect = JSON.parse('[0, 3, 130, [911992, 1, [6,  0]]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual('(Une fois tous les 7 tours)<br />911992<br />Disponible dès le tour 7');
+  });
+
+  it('should parse cooldowns with four items, pair = (0, 0) and values in array = (4, 4)', () => {
+    // GIVEN
+    const effect = JSON.parse(' [0, 3, 130, [507530, 0, [4,  4], 0]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual('(Une fois tous les 5 tours)<br />507530<br />Disponible dès le tour 5');
   });
 });
--- src/app/ffbe/mappers/effects/abilities/ability-cooldown-parser.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-cooldown-parser.ts
@@ -4,21 +4,44 @@ import {HTML_LINE_RETURN} from '../skill-effects.mapper';
 
 export class AbilityCooldownParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
+    const parameterError = 'Effet AbilityCooldownParser inconnu: Mauvaise liste de paramètres';
     if (effect.length < 4 || effect[0] !== 0 || effect[1] !== 3 ||
-      !Array.isArray(effect[3]) || effect[3].length < 3 || !Array.isArray(effect[3][2]) || effect[3][2].length !== 2) {
-      return 'Effet AbilityCooldownParser inconnu: Mauvaise liste de paramètres';
+      !Array.isArray(effect[3]) || effect[3].length < 3 || effect[3].length > 4 ||
+      !Array.isArray(effect[3][2]) || effect[3][2].length !== 2) {
+      return parameterError + ' (1)';
     }
 
     const content = effect[3];
 
     const activatedSkillId = content[0];
+    const prefix = content[1];
+    const innerArray = content[2];
+    const suffix = content.length > 3 ? content[3] : undefined;
+
     let available = 0;
     let cooldown = 0;
 
-    if (effect[3].length === 3) {
-      if (content[2][0] === content[2][1]) {
-        available = content[1];
-        cooldown = content[1] + content[2][0];
+    if (content.length === 3) {
+      if (prefix !== 1) {
+        return parameterError + ' (2)';
+      }
+      if (innerArray[0] === innerArray[1]) {
+        available = prefix;
+        cooldown = prefix + innerArray[0];
+      } else if (innerArray[1] === 0) {
+        available = prefix + innerArray[0];
+        cooldown = available;
+      } else {
+        return parameterError + ' (3)';
+      }
+    } else if (content.length === 4) {
+      if (prefix === 0 && suffix === 0) {
+        if (innerArray[0] === 4 && innerArray[1] === 4) {
+          available = innerArray[0] + 1;
+          cooldown = available;
+        } else {
+          return parameterError + ' (4)';
+        }
       }
     }
 
