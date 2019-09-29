ce8593dc9b0970850e4ec2582b2a8a59fc470b57
Sylvie <Sylvie@users.noreply.github.com>
2019-09-29 21:47:29 +0200

    Continued the AbilitySkillActivationParser
    Added a test for the activation of three skills for the caster.

--- src/app/ffbe/mappers/effects/abilities/ability-skill-activation.parser.spec.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-skill-activation.parser.spec.ts
@@ -35,10 +35,47 @@ describe('AbilitySkillActivationParser', () => {
     SkillsService['INSTANCE'] = skillsServiceMock;
     const mySpy = spyOn(skillsServiceMock, 'searchForSkillByGumiId').and.returnValues(Skill.produce(skill));
     // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, skill);
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
     // THEN
     expect(mySpy).toHaveBeenCalledTimes(1);
     expect(mySpy).toHaveBeenCalledWith(200200);
     expect(s).toEqual('Donne accès à <a href="ffexvius_skills.php?gumiid=200200">Coup de pied</a> pour 4 tours');
+  });
+
+  it('should parse activation of three skills for caster for N turns and no usage limit', () => {
+    // GIVEN
+    const skills = JSON.parse(ABILITY_SKILLS_TEST_DATA);
+    const names = JSON.parse(ABILITY_SKILLS_NAMES_TEST_DATA);
+    const descriptions = JSON.parse(ABILITY_SKILLS_SHORTDESCRIPTIONS_TEST_DATA);
+
+    const skill1: Skill = skills['200200'];
+    skill1.gumi_id = 200200;
+    skill1.names = names['200200'];
+    skill1.descriptions = descriptions['200200'];
+    const skill2: Skill = skills['200270'];
+    skill2.gumi_id = 200270;
+    skill2.names = names['200270'];
+    skill2.descriptions = descriptions['200270'];
+    const skill3: Skill = skills['202340'];
+    skill3.gumi_id = 202340;
+    skill3.names = names['202340'];
+    skill3.descriptions = descriptions['202340'];
+
+
+    const effect = JSON.parse('[0, 3, 100, [[2,  2,  2], [200200, 200270, 202340], 9999, 4, 1, 0]]');
+    const skillsServiceMock = new SkillsServiceMock() as SkillsService;
+    SkillsService['INSTANCE'] = skillsServiceMock;
+    const mySpy = spyOn(skillsServiceMock, 'searchForSkillByGumiId')
+      .and.returnValues(Skill.produce(skill1), Skill.produce(skill2), Skill.produce(skill3));
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(mySpy).toHaveBeenCalledTimes(3);
+    expect(mySpy).toHaveBeenCalledWith(200200);
+    expect(mySpy).toHaveBeenCalledWith(200270);
+    expect(mySpy).toHaveBeenCalledWith(202340);
+    expect(s).toEqual('Donne accès à <a href="ffexvius_skills.php?gumiid=200200">Coup de pied</a>, ' +
+      '<a href="ffexvius_skills.php?gumiid=200270">Transpercer</a>, ' +
+      '<a href="ffexvius_skills.php?gumiid=202340">Tir rapide</a> pour 3 tours');
   });
 });
