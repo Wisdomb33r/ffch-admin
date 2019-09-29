e2e4ccf28f6f17939e8198f10d7bba8e24c49a39
Wisdomb33r <info@final-fantasy.ch>
2019-09-29 17:15:28 +0200

    Fixing active multi-skill when given to an ally (Sylvie only for now).

--- src/app/ffbe/mappers/effects/abilities/ability-skill-multiple-activation.parser.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-skill-multiple-activation.parser.ts
@@ -23,17 +23,21 @@ export class AbilitySkillMultipleActivationParser extends EffectParser {
     const multiSkillAbilityActivatedId: number = effect[3][1];
     const modifiedSkillsIds: Array<number> = !Array.isArray(effect[3][3]) ? [effect[3][3]] : effect[3][3];
 
+    let target = '';
+    if (effect[0] === 1 && effect[1] === 2) {
+      target = ' à un allié';
+    }
 
     if (!this.isSkillFreeToCast(skill.cost) || skill.isActivatedByPassiveSkill) {
       const rawNumTurns: number = effect[3][4];
       if (rawNumTurns < 1) {
         return 'Effet AbilitySkillMultipleActivationParser inconnu: Nombre de tours incorrect (' + effect[3][4] + ')';
       }
-      const numTurns: number = skill.isActivatedByPassiveSkill ? rawNumTurns : rawNumTurns - 1;
+      const numTurns: number = skill.isActivatedByPassiveSkill || target.length > 0 ? rawNumTurns : rawNumTurns - 1;
       const pluralForm = numTurns > 1 ? 's' : '';
       const doubleSkillAbilityActivated: Skill = SkillsService.getInstance().searchForSkillByGumiId(multiSkillAbilityActivatedId);
       return 'Donne accès à ' + this.getSkillNameWithGumiIdentifierLink(doubleSkillAbilityActivated)
-        + ' pour ' + numTurns + ' tour' + pluralForm;
+        + ' pour ' + numTurns + ' tour' + pluralForm + target;
     } else {
       const modifiedSkills = modifiedSkillsIds.map((skillId: number) => SkillsService.getInstance().searchForSkillByGumiId(skillId));
       return 'Permet l\'utilisation de ' + this.getSkillsNamesWithGumiIdentifierLinks(modifiedSkills) + ' ' + nbTimes + 'x par tour';
