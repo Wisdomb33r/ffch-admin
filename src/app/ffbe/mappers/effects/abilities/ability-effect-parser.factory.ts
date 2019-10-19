e7071bae6c9872bc83db5a6951b17fcc188e3201
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-10-19 14:55:39 +0200

    #64: Added the AbilityLbDamageIncreaseParser
    This was the missing parser for Christine.

--- src/app/ffbe/mappers/effects/abilities/ability-effect-parser.factory.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-effect-parser.factory.ts
@@ -51,6 +51,7 @@ import {AbilityProvocationsParser} from './ability-provocations.parser';
 import {AbilityDebuffsCureParser} from './ability-debuffs-cure.parser';
 import {AbilityEsperOrbsParser} from './ability-esper-orbs.parser';
 import {AbilityCoversParser} from './ability-covers.parser';
+import {AbilityLbDamageIncreaseParser} from './ability-lb-damage-increase.parser';
 
 export class AbilityEffectParserFactory {
   public static getParser(effectId1: number, effectId2: number, effectId3: number): EffectParser {
@@ -175,6 +176,8 @@ export class AbilityEffectParserFactory {
         return new AbilityDamagesMagicSprScalingParser();
       case 111:
         return new AbilityDebuffsCureParser();
+      case 120:
+        return new AbilityLbDamageIncreaseParser();
       case 124:
         return new AbilityDamagesEvokerParser();
       case 125:
---
+++ src/app/ffbe/mappers/effects/abilities/ability-lb-damage-increase.parser.ts
@@ -0,0 +1,22 @@
+import {EffectParser} from '../effect-parser';
+import {Skill} from '../../../model/skill.model';
+
+export class AbilityLbDamageIncreaseParser extends EffectParser {
+  public parse(effect: Array<any>, skill: Skill): string {
+    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 2) {
+      return 'Effet AbilityLbDamageIncreaseParser inconnu: Mauvaise liste de paramètres';
+    }
+
+    const damage: string = effect[3][0].toString();
+    const numTurns = effect[3][1];
+    const target = this.getTarget(effect[0], effect[1], 'TargetWithPreposition.De');
+    const pluralForm = numTurns > 1 ? 's' : '';
+
+    let dispellable = '';
+    if (effect[3].length >= 4 && effect[3][3] === 0) {
+      dispellable = ' (bonus non-dissipable)';
+    }
+
+    return `+${damage}% aux dégâts de la limite ${target} pour ${numTurns} tour${pluralForm}${dispellable}`;
+  }
+}
