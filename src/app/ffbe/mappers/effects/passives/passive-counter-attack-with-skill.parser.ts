9813b5f0e8d815b94deac32c7d2ebe52083379fa
Wisdomb33r <info@final-fantasy.ch>
2019-10-21 14:06:03 +0200

    Fixing max-per-turn optional parameter for skills used as counter.

--- src/app/ffbe/mappers/effects/passives/passive-counter-attack-with-skill.parser.ts
+++ src/app/ffbe/mappers/effects/passives/passive-counter-attack-with-skill.parser.ts
@@ -6,26 +6,29 @@ import {HTML_LINE_RETURN} from '../skill-effects.mapper';
 
 export class PassiveCounterAttackWithSkillParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
-    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 4) {
+    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 3) {
       return 'Effet PassiveCounterAttackWithSkillParser inconnu: Mauvaise liste de paramètres';
     }
 
     const counterChance = effect[3][0];
-    const targets = effect[3][1]; // TODO how to integrate this into the description of the activated skill ? -_-
+    // TODO how to integrate effect[3][1], which represents the target, into the description of the activated skill ? -_-
     const activatedSkillId = effect[3][2];
     const maxActivationNumber = effect[3][3];
     const damageTypeText = effect[2] === 49 ? 'physiques' : 'magiques';
-    const prefixText = counterChance + '% de chance de contre-attaquer les dégâts ' + damageTypeText + ' par: ';
-    const suffixText = ' (max ' + maxActivationNumber + ' par tour)';
+    const prefixText = `${counterChance}% de chance de contre-attaquer les dégâts ${damageTypeText} par: `;
+    let suffixText = '';
+    if (maxActivationNumber) {
+      suffixText = ` (max ${maxActivationNumber} par tour)`;
+    }
     const activatedSkill: Skill = SkillsService.getInstance().searchForSkillByGumiId(activatedSkillId);
     if (!activatedSkill) {
-      return prefixText + 'UNKNOWN skill' + suffixText;
+      return `${prefixText} UNKNOWN skill ${suffixText}`;
     }
 
     activatedSkill.isActivatedByPassiveSkill = true;
     return SkillMapper.toCompetence(activatedSkill).effet_fr
       .split(HTML_LINE_RETURN)
-      .map(effet => prefixText + effet + suffixText)
+      .map(effet => `${prefixText}${effet}${suffixText}`)
       .join(HTML_LINE_RETURN);
   }
 }
