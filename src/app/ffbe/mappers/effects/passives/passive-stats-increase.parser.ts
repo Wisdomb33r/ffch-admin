e2a592f27aa74c3b369d0757c83f4ef9e326a63c
Wisdomb33r <info@final-fantasy.ch>
2019-10-28 09:35:50 +0100

    Stats increase now has wording for passive critical hit chance of normal attacks (#86).

--- src/app/ffbe/mappers/effects/passives/passive-stats-increase.parser.ts
+++ src/app/ffbe/mappers/effects/passives/passive-stats-increase.parser.ts
@@ -1,6 +1,7 @@
 import {EffectParser} from '../effect-parser';
 import {Skill} from '../../../model/skill.model';
 import {FfbeUtils} from '../../../utils/ffbe-utils';
+import {HTML_LINE_RETURN} from '../skill-effects.mapper';
 
 export class PassiveStatsIncreaseParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
@@ -15,11 +16,19 @@ export class PassiveStatsIncreaseParser extends EffectParser {
       {name: 'MAG', value: effect[3][2]},
       {name: 'PSY', value: effect[3][3]},
     ];
-    // TODO critical strikes
-    return FfbeUtils.replaceLastOccurenceInString(this.wordEffectJoiningIdenticalValues(increases), ', ', ' et ');
+    const increasesText = FfbeUtils.replaceLastOccurenceInString(this.wordEffectJoiningIdenticalValues(increases), ', ', ' et ');
+    const critChance = effect[3][6];
+    let critChanceText = '';
+    if (critChance > 0) {
+      critChanceText = `+${critChance}% de coups critiques des attaques normales`;
+    }
+    if (increasesText && increasesText.length && critChanceText && critChanceText.length) {
+      critChanceText = HTML_LINE_RETURN + critChanceText;
+    }
+    return increasesText + critChanceText;
   }
 
   protected wordEffectForIdenticalValues(currentValue, accumulatedStats: Array<string>): string {
-    return '+' + currentValue + '% ' + accumulatedStats.join('/');
+    return `+${currentValue}% ${accumulatedStats.join('/')}`;
   }
 }
