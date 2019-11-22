8d2f53ba874bcbe341ff1c8e7ac069c3378a50d7
Wisdomb33r <info@final-fantasy.ch>
2019-11-22 17:29:00 +0100

    Healing mod increase calculation (#139).

--- src/app/ffbe/mappers/effects/abilities/ability-skill-modifier-increase.parser.spec.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-skill-modifier-increase.parser.spec.ts
@@ -73,4 +73,25 @@ describe('AbilitySkillModifierIncreaseParser', () => {
     expect(s).toEqual('+600% de puissance à <a href="ffexvius_skills.php?gumiid=509024">Météore X</a> à un allié pour 5 tours (ID #1214)'
       + HTML_LINE_RETURN + '+400% de puissance à <a href="ffexvius_skills.php?gumiid=20430">Météore</a> à un allié pour 5 tours (ID #1214)');
   });
+
+  it('should parse skill modifier increase for healing', () => {
+    // GIVEN
+    const skills = JSON.parse(MAGIC_SKILLS_TEST_DATA);
+    const names = JSON.parse(MAGIC_SKILLS_NAMES_TEST_DATA);
+    const descriptions = JSON.parse(MAGIC_SKILLS_SHORTDESCRIPTIONS_TEST_DATA);
+    const skill1: Skill = skills['10020'];
+    skill1.gumi_id = 10020;
+    skill1.active = true;
+    skill1.names = names['10020'];
+    skill1.descriptions = descriptions['10020'];
+
+    const effect = JSON.parse('[1, 2, 136, [[10020], 0, 0, 300, 5, 1, 1214, 1]]');
+    const skillsServiceMock = new SkillsServiceMock() as SkillsService;
+    SkillsService['INSTANCE'] = skillsServiceMock;
+    spyOn(skillsServiceMock, 'searchForSkillByGumiId').and.returnValues(Skill.produce(skill1));
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual('+1.5x la PSY + 0.3x la MAG de puissance à <a href="ffexvius_skills.php?gumiid=10020">Soin</a> à un allié pour 5 tours (ID #1214)');
+  });
 });
--- src/app/ffbe/mappers/effects/passives/passive-skill-modifier-increase.parser.ts
+++ src/app/ffbe/mappers/effects/passives/passive-skill-modifier-increase.parser.ts
@@ -4,6 +4,9 @@ import {SkillsService} from '../../../services/skills.service';
 import {HTML_LINE_RETURN} from '../skill-effects.mapper';
 
 export class PassiveSkillModifierIncreaseParser extends EffectParser {
+
+  private modifiedSkillsIncreases: Array<{ name: string, value: number, isHeal: boolean }> = [];
+
   public parse(effect: Array<any>, skill: Skill): string {
     if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 4 || effect[3][1] !== 0 || effect[3][2] !== 0) {
       return 'Effet PassiveSkillModifierIncreaseParser inconnu: Mauvaise liste de paramètres';
@@ -12,21 +15,37 @@ export class PassiveSkillModifierIncreaseParser extends EffectParser {
     const modifiedSkills = !Array.isArray(effect[3][0]) ? [effect[3][0]] : effect[3][0];
     const skillModifierIncrease = effect[3][3];
 
-    const modifiedSkillsIncreases: Array<{ name: string, value: number }> = [];
     modifiedSkills.forEach(skillId => {
       const activatedSkill: Skill = SkillsService.getInstance().searchForSkillByGumiId(skillId);
-      modifiedSkillsIncreases.push({
+      const modIncrease = !activatedSkill ? 0 : activatedSkill.calculateTotalModIncrease(skillModifierIncrease);
+      const healingModIncrease = !activatedSkill ? 0 : activatedSkill.calculateHealingTotalModIncrease(skillModifierIncrease);
+      this.modifiedSkillsIncreases.push({
         name: this.getSkillNameWithGumiIdentifierLink(activatedSkill),
-        value: !activatedSkill ? 0 : activatedSkill.calculateTotalModIncrease(skillModifierIncrease),
+        value: modIncrease > 0 ? modIncrease : healingModIncrease,
+        isHeal: modIncrease === 0 && healingModIncrease > 0,
       });
     });
 
-    return this.wordEffectJoiningIdenticalValues(modifiedSkillsIncreases, HTML_LINE_RETURN, true);
+    const modIncreaseText = this.wordEffectJoiningIdenticalValues(
+      this.modifiedSkillsIncreases.filter(increase => !increase.isHeal), HTML_LINE_RETURN, true);
+    const healingModIncreaseText = this.wordEffectJoiningIdenticalValues(
+      this.modifiedSkillsIncreases.filter(increase => increase.isHeal), HTML_LINE_RETURN, true);
+    const modIncreasesJoiningText = modIncreaseText.length && healingModIncreaseText.length ? HTML_LINE_RETURN : '';
+    return `${modIncreaseText}${modIncreasesJoiningText}${healingModIncreaseText}`;
   }
 
   protected wordEffectForIdenticalValues(currentValue, accumulatedStats: Array<string>): string {
-    const displayedValue = (currentValue > 0 ? Math.round(currentValue) : 'UNKNOWN');
+    const isHeal: boolean = this.modifiedSkillsIncreases.find(increase => increase.name === accumulatedStats[0]).isHeal;
+    let valueText: string;
+    let percentText = '';
+    if (isHeal) {
+      valueText = `${currentValue / 200}x la PSY + ${currentValue / 1000}x la MAG`;
+    } else {
+      valueText = `${Math.round(currentValue)}`;
+      percentText = '%';
+    }
+    const displayedValue = (currentValue > 0 ? valueText : 'UNKNOWN');
     const skillsText = accumulatedStats.join(', ');
-    return `+${displayedValue}% de puissance à ${skillsText}`;
+    return `+${displayedValue}${percentText} de puissance à ${skillsText}`;
   }
 }
