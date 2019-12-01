2f3b792bef9156c8dd4bd85fac59b259d08bae73
Sylvie Stucki <Sylvie@users.noreply.github.com>
2019-12-01 11:59:04 +0100

    #159: Added two types of effects to the AbilityCopyEffectsParser
    The new effects are:
    - Physical mitigation
    - Magical mitigation
    
    These effect were taken from the "EffectParser" where we already identified them.

--- src/app/ffbe/mappers/effects/abilities/ability-copy-effects.parser.spec.ts
+++ src/app/ffbe/mappers/effects/abilities/ability-copy-effects.parser.spec.ts
@@ -3,36 +3,38 @@ import {HTML_LINE_RETURN} from '../skill-effects.mapper';
 
 describe('AbilityCopyEffectsParser', () => {
 
-  it('should parse effect copy from one enemy to the caster (inspired from "Grand Reversal")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 1, 1005, [0, 3, 100, "1;2;3;4;39;40;41;42"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un adversaire au lanceur pour 3 tours:` +
-      `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
-  });
+  it('should parse effect copy from one enemy to the caster (inspired from "Grand Reversal")',
+    () => {
+      // GIVEN
+      const effect = JSON.parse('[1, 1, 1005, [0, 3, 100, "1;2;3;4;39;40;41;42"]]');
+      // WHEN
+      const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+      // THEN
+      expect(s).toEqual(`Copie les effets suivants d\'un adversaire au lanceur pour 3 tours:` +
+        `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
+    });
 
-  it('should parse effect copy from one enemy to the caster for 1 turn (inspired from "Grand Reversal")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 1, 1005, [0, 1, 100, "1;2;3;4;39;40;41;42"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un adversaire au lanceur pour 1 tour:` +
-      `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
-  });
+  it('should parse effect copy from one enemy to the caster for 1 turn (inspired from "Grand Reversal")',
+    () => {
+      // GIVEN
+      const effect = JSON.parse('[1, 1, 1005, [0, 1, 100, "1;2;3;4;39;40;41;42"]]');
+      // WHEN
+      const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+      // THEN
+      expect(s).toEqual(`Copie les effets suivants d\'un adversaire au lanceur pour 1 tour:` +
+        `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
+    });
 
-  it('should parse effect copy from the caster to the rest of the party (inspired from "Projected Mirror")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 3, 1005, [1, 4, 100, "1;2;3;4"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants du lanceur aux alliés sauf le lanceur pour 4 tours:` +
-      `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
-  });
-
+  it('should parse effect copy from the caster to the rest of the party (inspired from "Projected Mirror")',
+    () => {
+      // GIVEN
+      const effect = JSON.parse('[1, 3, 1005, [1, 4, 100, "1;2;3;4"]]');
+      // WHEN
+      const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+      // THEN
+      expect(s).toEqual(`Copie les effets suivants du lanceur aux alliés sauf le lanceur pour 4 tours:` +
+        `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
+    });
 
   it('should parse effect copy from one enemy to the party (inspired from "Twist of Fate")', () => {
     // GIVEN
@@ -44,35 +46,25 @@ describe('AbilityCopyEffectsParser', () => {
       `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY`);
   });
 
-  it('should parse copy of elemental resistances and imbues effects ("Elemental Mirror")', () => {
+  it('should parse copy of magic damage resistance (inspired from "Twist of Fate")', () => {
     // GIVEN
-    const effect = JSON.parse('[1, 2, 1005, [0, 5, 100, "23;24;25;26;27;28;29;30;87;88;89;90;91;92;93;94"]]');
+    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "9"]]');
     // WHEN
     const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
     // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 5 tours:` +
-      `${HTML_LINE_RETURN}Bonus aux rés. élémentaires${HTML_LINE_RETURN}` +
-      `Éléments ajoutés aux attaques physiques et hybrides`);
+    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
+      `${HTML_LINE_RETURN}Rés. aux dégâts magiques`);
   });
 
-  it('should parse copy of bonus to LB damages ("Critical Mirror")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 2, 1005, [0,  2,  100,  221]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 2 tours:` +
-      `${HTML_LINE_RETURN}Bonus aux dégâts de la limite`);
-  });
 
-  it('should parse copy of physical and magical killers ("Critical Mirror")', () => {
+  it('should parse copy of status ailments resistance (inspired from "Survival Mirror")', () => {
     // GIVEN
-    const effect = JSON.parse('[1, 2, 1005, [0, 5, 100, "63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;1;2;3;4;39;40;41;42"]]');
+    const effect = JSON.parse('[1, 2, 1005, [0, 3, 100, "12;13;14;15;16;17;18;19"]]');
     // WHEN
     const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
     // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 5 tours:` +
-      `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY${HTML_LINE_RETURN}Tueurs physiques${HTML_LINE_RETURN}Tueurs magiques`);
+    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 3 tours:` +
+      `${HTML_LINE_RETURN}Rés. aux altérations`);
   });
 
 
@@ -86,26 +78,6 @@ describe('AbilityCopyEffectsParser', () => {
       `${HTML_LINE_RETURN}Régénération de PV par tour${HTML_LINE_RETURN}Régénération de PM par tour`);
   });
 
-  it('should parse copy of status ailments resistance (inspired from "Survival Mirror")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 2, 1005, [0, 3, 100, "12;13;14;15;16;17;18;19"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 3 tours:` +
-      `${HTML_LINE_RETURN}Rés. aux altérations`);
-  });
-
-  it('should parse copy of stats breaks, stop and charm (inspired from "Survival Mirror")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 2, 1005, [0, 3, 100, "95;96;97;98;99;100"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 3 tours:` +
-      `${HTML_LINE_RETURN}Rés. aux baisses de caractéristiques, à Stop et à Charme`);
-  });
-
   it('should parse copy of Auto-Revive (inspired from "Survival Mirror")', () => {
     // GIVEN
     const effect = JSON.parse('[1, 2, 1005, [0, 3, 100, "21"]]');
@@ -114,6 +86,17 @@ describe('AbilityCopyEffectsParser', () => {
     // THEN
     expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 3 tours:` +
       `${HTML_LINE_RETURN}Auréole`);
+  });
+
+  it('should parse copy of elemental resistances and imbues effects ("Elemental Mirror")', () => {
+    // GIVEN
+    const effect = JSON.parse('[1, 2, 1005, [0, 5, 100, "23;24;25;26;27;28;29;30;87;88;89;90;91;92;93;94"]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 5 tours:` +
+      `${HTML_LINE_RETURN}Bonus aux rés. élémentaires${HTML_LINE_RETURN}` +
+      `Éléments ajoutés aux attaques physiques et hybrides`);
   });
 
   it('should parse copy of LB jauge fill rate (inspired from "Survival Mirror")', () => {
@@ -126,16 +109,6 @@ describe('AbilityCopyEffectsParser', () => {
       `${HTML_LINE_RETURN}Bonus à la vitesse de la jauge de limite`);
   });
 
-  it('should parse copy of magic damage resistance (inspired from "Twist of Fate")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "9"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
-      `${HTML_LINE_RETURN}Rés. aux dégâts magiques`);
-  });
-
   it('should parse copy of physical dodge (inspired from "Twist of Fate")', () => {
     // GIVEN
     const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "56"]]');
@@ -144,6 +117,26 @@ describe('AbilityCopyEffectsParser', () => {
     // THEN
     expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
       `${HTML_LINE_RETURN}Esquive d\'attaques physiques`);
+  });
+
+  it('should parse copy of physical mitigation (inspired from "Twist of Fate")', () => {
+    // GIVEN
+    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "57"]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
+      `${HTML_LINE_RETURN}Mitigation physique`);
+  });
+
+  it('should parse copy of physical mitigation (inspired from "Twist of Fate")', () => {
+    // GIVEN
+    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "58"]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
+      `${HTML_LINE_RETURN}Mitigation magique`);
   });
 
   it('should parse copy of magic reflects (inspired from "Twist of Fate")', () => {
@@ -166,6 +159,28 @@ describe('AbilityCopyEffectsParser', () => {
       `${HTML_LINE_RETURN}Bonus aux dégâts`);
   });
 
+  it('should parse copy of physical and magical killers ("Critical Mirror")', () => {
+    // GIVEN
+    const effect = JSON.parse('[1, 2, 1005, [0, 5, 100, ' +
+      '"63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;1;2;3;4;39;40;41;42"]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 5 tours:` +
+      `${HTML_LINE_RETURN}Bonus d'ATT/DÉF/MAGIE/PSY${HTML_LINE_RETURN}Tueurs physiques` +
+      `${HTML_LINE_RETURN}Tueurs magiques`);
+  });
+
+  it('should parse copy of stats breaks, stop and charm (inspired from "Survival Mirror")', () => {
+    // GIVEN
+    const effect = JSON.parse('[1, 2, 1005, [0, 3, 100, "95;96;97;98;99;100"]]');
+    // WHEN
+    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    // THEN
+    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 3 tours:` +
+      `${HTML_LINE_RETURN}Rés. aux baisses de caractéristiques, à Stop et à Charme`);
+  });
+
   it('should parse copy of Berserk resistance (inspired from "Twist of Fate")', () => {
     // GIVEN
     const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "101"]]');
@@ -176,24 +191,14 @@ describe('AbilityCopyEffectsParser', () => {
       `${HTML_LINE_RETURN}Rés. à Berserk`);
   });
 
-  it('should parse copy of physical mitigation (inspired from "Twist of Fate")', () => {
+  it('should parse copy of bonus to LB damages ("Critical Mirror")', () => {
     // GIVEN
-    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "57"]]');
+    const effect = JSON.parse('[1, 2, 1005, [0,  2,  100,  221]]');
     // WHEN
     const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
     // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
-      `${HTML_LINE_RETURN}Mitigation physique`);
-  });
-
-  it('should parse copy of physical mitigation (inspired from "Twist of Fate")', () => {
-    // GIVEN
-    const effect = JSON.parse('[1, 1, 1005, [2, 2, 100, "58"]]');
-    // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
-    // THEN
-    expect(s).toEqual(`Copie les effets suivants d\'un adversaire aux alliés pour 2 tours:` +
-      `${HTML_LINE_RETURN}Mitigation magique`);
+    expect(s).toEqual(`Copie les effets suivants d\'un allié au lanceur pour 2 tours:` +
+      `${HTML_LINE_RETURN}Bonus aux dégâts de la limite`);
   });
 
 });
