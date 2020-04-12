659e22c57835da5bb92becb47efa18e4685e681a
Sylvie Stucki <Sylvie@users.noreply.github.com>
2020-04-12 09:34:19 +0200

    #207: Added the PassiveAilmentsCureAfterBattleParser
    This parses the passive abilities for curing status ailments after battle.

---
+++ src/app/ffbe/mappers/effects/passives/passive-ailments-cure-after-battle.parser.ts
@@ -0,0 +1,31 @@
+import {EffectParser} from '../effect-parser';
+import {Skill} from '../../../model/skill.model';
+import {FfbeUtils} from '../../../utils/ffbe-utils';
+
+export class PassiveAilmentsCureAfterBattleParser extends EffectParser {
+  public parse(effect: Array<any>, skill: Skill): string {
+    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 8 || effect[3].reduce((a, b) => a + b, 0) < 1) {
+      return 'Effet PassiveAilmentsCureAfterBattleParser inconnu: Mauvaise liste de paramètres';
+    }
+
+    const target = this.getTarget(effect[0], effect[1]);
+
+    const cures = effect[3].map((value, index) => {
+      if (value === 1) return (this.getAilmentFromId(index + 1));
+    })
+      .filter(value => !FfbeUtils.isNullOrUndefined(value));
+
+    let curesText = '';
+    if (cures.length === 0) {
+      curesText = 'UNKNOWN ailments';
+    } else if (cures.length === 8) {
+      curesText = 'toutes les altérations';
+    } else {
+      curesText = FfbeUtils.replaceLastOccurenceInString(cures.join(', '), ', ', ' et ');
+    }
+
+    return `Soigne ${curesText} ${target} après le combat`;
+  }
+
+//
+}
