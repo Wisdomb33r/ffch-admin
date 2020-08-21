bca3da8aaef4e068e9e0e6c771e629df73a3bfe6
Sylvie Stucki <Sylvie@users.noreply.github.com>
2020-08-21 19:18:24 +0200

    #253: Moved the CharacterSkillsDisplay into the CharacterDisplay
    This belongs to the CharacterDisplay.

--- src/app/ffbe/character-display/character-display.component.ts
+++ src/app/ffbe/character-display/character-display.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Personnage} from '../model/personnage.model';
+import {Competence} from '../model/competence.model';
 
 @Component({
   selector: 'app-character-display',
@@ -9,11 +10,19 @@ import {Personnage} from '../model/personnage.model';
 export class CharacterDisplayComponent implements OnInit {
 
   @Input() personnage: Personnage;
+  competences: Array<Competence>;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.competences = [];
+    this.personnage.unites[this.personnage.unites.length - 1].competences
+      .forEach(uniteCompetence => this.competences.push(uniteCompetence.competence));
+  }
+
+  public areCharacterSkillsDisplayed(): boolean {
+    return Array.isArray(this.competences) && this.competences.length > 0;
   }
 
 }
--- src/app/ffbe/characters/characters.component.ts
+++ src/app/ffbe/characters/characters.component.ts
@@ -6,7 +6,6 @@ import {LimitBurstsService} from '../services/limit-bursts.service';
 import {SkillsService} from '../services/skills.service';
 import {Character} from '../model/character.model';
 import {CharacterMapper} from '../mappers/character-mapper';
-import {Competence} from '../model/competence.model';
 import {EquipmentsService} from '../services/equipments.service';
 import {MateriasService} from '../services/materias.service';
 import {FfbeUtils} from '../utils/ffbe-utils';
@@ -21,7 +20,6 @@ export class CharactersComponent implements OnInit {
 
   name: FormControl;
   personnage: Personnage;
-  competences: Array<Competence>;
 
   constructor(private charactersService: CharactersService,
               private limitBurstsService: LimitBurstsService,
@@ -38,21 +36,14 @@ export class CharactersComponent implements OnInit {
 
   public searchCharacterInDataMining() {
     this.personnage = null;
-    this.competences = [];
     const character: Character = this.charactersService.searchForCharacterByNameOrGumiId(this.name.value);
     if (character) {
       this.personnage = CharacterMapper.toPersonnage(character);
-      this.personnage.unites[this.personnage.unites.length - 1].competences
-        .forEach(uniteCompetence => this.competences.push(uniteCompetence.competence));
     }
   }
 
   public isCharacterDisplayed(): boolean {
     return !FfbeUtils.isNullOrUndefined(this.personnage);
-  }
-
-  public isCharacterSkillsDisplayed(): boolean {
-    return Array.isArray(this.competences) && this.competences.length > 0;
   }
 
   public isDataMiningLoading(): boolean {
