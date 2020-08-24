6f098d3771cdf2630e6d4bd86d07a1d9d078fe39
Sylvie Stucki <Sylvie@users.noreply.github.com>
2020-08-24 20:32:26 +0200

    #267: Added methods to compute the sum of an Array of Caracteristiques
    Also added tests.

---
+++ src/app/ffbe/model/caracteristiques.model.spec.ts
@@ -0,0 +1,67 @@
+import {Caracteristiques} from './caracteristiques.model';
+
+describe('Caracteristiques', () => {
+  it('should add two Caracteristiques correctly', () => {
+    // GIVEN
+    const carac1 = new Caracteristiques(1, 2, 3, 4, 5, 6);
+    const carac2 = new Caracteristiques(60, 70, 80, 90, 100, 110);
+
+    // WHEN
+    carac1.accumulateByAddition(carac2);
+
+    // THEN
+    expect(carac1).toEqual(new Caracteristiques(61, 72, 83, 94, 105, 116));
+  });
+
+  it('should add one valid and one null Caracteristiques correctly', () => {
+    // GIVEN
+    const carac1 = new Caracteristiques(1, 2, 3, 4, 5, 6);
+    const carac2 = null;
+
+    // WHEN
+    carac1.accumulateByAddition(carac2);
+
+    // THEN
+    expect(carac1).toEqual(new Caracteristiques(1, 2, 3, 4, 5, 6));
+  });
+
+  it('should reduce three Caracteristiques by addition correctly', () => {
+    // GIVEN
+    const carac1 = new Caracteristiques(1, 2, 3, 4, 5, 6);
+    const carac2 = new Caracteristiques(60, 70, 80, 90, 100, 110);
+    const carac3 = new Caracteristiques(100, 200, 300, 400, 500, 600);
+
+    const caracs = [carac1, carac2, carac3];
+
+    // WHEN
+    const result = Caracteristiques.computeSum(caracs);
+
+    // THEN
+    expect(carac1).toEqual(new Caracteristiques(1, 2, 3, 4, 5, 6));
+    expect(carac2).toEqual(new Caracteristiques(60, 70, 80, 90, 100, 110));
+    expect(carac3).toEqual(new Caracteristiques(100, 200, 300, 400, 500, 600));
+
+    expect(result).toEqual(new Caracteristiques(161, 272, 383, 494, 605, 716));
+  });
+
+  it('should reduce many Caracteristiques with null entries by addition correctly', () => {
+    // GIVEN
+    const carac1 = new Caracteristiques(1, 2, 3, 4, 5, 6);
+    const carac2 = new Caracteristiques(60, 70, 80, 90, 100, 110);
+    const carac3 = new Caracteristiques(100, 200, 300, 400, 500, 600);
+
+    const caracs = [null, carac1, null, carac2, null, carac3, null];
+
+    // WHEN
+    const result = Caracteristiques.computeSum(caracs);
+
+    // THEN
+    expect(carac1).toEqual(new Caracteristiques(1, 2, 3, 4, 5, 6));
+    expect(carac2).toEqual(new Caracteristiques(60, 70, 80, 90, 100, 110));
+    expect(carac3).toEqual(new Caracteristiques(100, 200, 300, 400, 500, 600));
+
+    expect(result).toEqual(new Caracteristiques(161, 272, 383, 494, 605, 716));
+  });
+
+});
+
--- src/app/ffbe/model/caracteristiques.model.ts
+++ src/app/ffbe/model/caracteristiques.model.ts
@@ -1,3 +1,5 @@
+import {FfbeUtils} from '../utils/ffbe-utils';
+
 export class Caracteristiques {
   public constructor(
     public pv: number,
@@ -17,4 +19,21 @@ export class Caracteristiques {
     return new Caracteristiques(oc.pv, oc.pm, oc.att, oc.def, oc.mag, oc.psy);
   }
 
+  public accumulateByAddition(other: Caracteristiques): Caracteristiques {
+    if (FfbeUtils.isNullOrUndefined(other)) {
+      return this;
+    }
+
+    this.pv += other.pv;
+    this.pm += other.pm;
+    this.att += other.att;
+    this.def += other.def;
+    this.mag += other.mag;
+    this.psy += other.psy;
+    return this;
+  }
+
+  public static computeSum(entries: Array<Caracteristiques>): Caracteristiques {
+    return entries.reduce((a, b) => a.accumulateByAddition(b), Caracteristiques.newEmptyCaracteristiques());
+  }
 }
