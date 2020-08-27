2565096e5be903da049870c875459fadb22d5f3a
Wisdomb33r <info@final-fantasy.ch>
2020-08-27 13:56:06 +0200

    NV magnus abilities now parsing multiple usage per turn limitation (#260).

--- src/app/ffbe/model/effects/abilities/skill/ability-skill-magnus-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/skill/ability-skill-magnus-effect.model.ts
@@ -12,18 +12,21 @@ export class AbilitySkillMagnusEffect extends SkillEffect {
   private activatedSkillId: number;
   private activatedSkill: Skill;
   private activationLimit: number;
+  private multipleUsagePerTurn: boolean;
 
   constructor(protected targetNumber: TargetNumberEnum,
               protected targetType: TargetTypeEnum,
               protected effectId: number,
               protected parameters: Array<any>) {
     super(targetNumber, targetType, effectId);
-    if (!Array.isArray(parameters) || parameters.length < 9) {
+    if (!Array.isArray(parameters) || parameters.length < 9 || parameters[1] !== 1 || parameters[4] !== 1 || parameters[5] !== 1
+      || parameters[6] !== 1 || (parameters[7] !== 0 && parameters[7] !== 1)) {
       this.parameterError = true;
     } else {
       this.activatedSkillId = parameters[0];
       this.activatedSkill = SkillsService.getInstance().searchForSkillByGumiId(this.activatedSkillId);
       this.activationLimit = parameters[2];
+      this.multipleUsagePerTurn = parameters[7] !== 0;
     }
   }
 
@@ -31,7 +34,8 @@ export class AbilitySkillMagnusEffect extends SkillEffect {
     const activatedSkillText = SkillMapper.toCompetence(this.activatedSkill).effet_fr;
     EffectParser.fillSkillWithTransitiveActivatedSkillInformation(skill, this.activatedSkill);
     const pluralSuffix = this.activationLimit > 1 ? 's' : '';
-    const activationLimitText = `<strong>Seulement ${this.activationLimit} utilisation${pluralSuffix} par combat:</strong>`;
+    const multipleUsageText = this.multipleUsagePerTurn || this.activationLimit < 2 ? '' : ' (une seule utilisation par multi-cast)';
+    const activationLimitText = `<strong>${this.activationLimit} utilisation${pluralSuffix} par combat</strong>${multipleUsageText}:`;
     return `${activationLimitText}${HTML_LINE_RETURN}${activatedSkillText}`;
   }
 
