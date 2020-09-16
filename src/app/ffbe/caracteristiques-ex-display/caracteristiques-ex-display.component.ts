3dbf3eafb811bdc8a4b4d9ae1724437216c07e2e
Sylvie <Sylvie@users.noreply.github.com>
2020-09-16 21:50:07 +0200

    #286: Added skeleton of CaracteristiquesEXDisplay component
    The UnitDisplay components displays it for Neo-Vision and Brave-Shift units that have a caracEx field.

---
+++ src/app/ffbe/caracteristiques-ex-display/caracteristiques-ex-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caracteristiques-ex-display',
+  templateUrl: './caracteristiques-ex-display.component.html',
+  styleUrls: ['./caracteristiques-ex-display.component.css']
+})
+export class CaracteristiquesEXDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
