4b46a2db3d42f74c44ae01daeaf06302e711cd9b
Sylvie <Sylvie@users.noreply.github.com>
2020-09-20 09:12:59 +0200

    #307: Renamed AbilityDamageIncreasedBreakEffect to AbilityDamagePhysicalIncreasedBreakEffect
    The type of attack is always physical (so far).

--- src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.ts
@@ -1,13 +1,13 @@
 import {SkillEffect} from '../../skill-effect.model';
 import {Skill} from '../../../skill.model';
 
-export class AbilityDamageIncreasedBreakEffect extends SkillEffect {
+export class AbilityDamagePhysicalIncreasedBreakEffect extends SkillEffect {
   protected get effectName(): string {
     return '';
   }
 
   protected wordEffectImpl(skill: Skill): string {
-    return 'AbilityDamageIncreasedBreakEffect';
+    return 'AbilityDamagePhysicalIncreasedBreakEffect';
   }
 
 }
