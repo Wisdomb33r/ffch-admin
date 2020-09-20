8c95d6f28c018b7521b4e03c7d53ef5b63af40af
Sylvie Stucki <Sylvie@users.noreply.github.com>
2020-09-20 12:39:06 +0200

    #307: Partial implementation of AbilityDamagePhysicalIncreasedBreakEffect
    The equipment type is not taken into account yet.

--- src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.spec.ts
+++ src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.spec.ts
@@ -1,14 +1,19 @@
 import {AbilitySkillEffectFactory} from '../../ability-skill-effect.factory';
+import {Skill} from '../../../skill.model';
 
 describe('AbilityDamagePhysicalIncreasedBreakEffect', () => {
 
   it('should parse damages with bonus BREAK provided by a single weapon type', () => {
     // GIVEN
     const effect = JSON.parse('[1, 1, 159, [4,  400,  1,  100,  0,  1]]');
+    const fakeSkill: Skill = new Skill();
+    fakeSkill.element_inflict = null;
+    fakeSkill.attack_type = 'Physical';
+
     // WHEN
-    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(null);
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(fakeSkill);
     // THEN
-    expect(s).toEqual('Physical damage (1x, ATK) with bonus BREAK damage when equipped with a Katana (400%) to one enemy');
+    expect(s).toEqual('Dégâts physiques neutres de puissance 100% à un adversaire avec un bonus BREAK de 400% si l\'unité porte un <a href="ffexvius_objects.php?categid=28">katana</a>');
   });
 
 });
--- src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/damage/ability-damage-physical-increased-break-effect.model.ts
@@ -5,17 +5,22 @@ import {TargetTypeEnum} from '../../target-type.enum';
 
 export class AbilityDamagePhysicalIncreasedBreakEffect extends SkillEffect {
 
-  protected power: number;
+  protected basePower: number;
+  protected bonusPower: number;
+  protected equipmentGumiId: number;
 
   constructor(protected targetNumber: TargetNumberEnum,
               protected targetType: TargetTypeEnum,
               protected effectId: number,
               protected parameters: Array<any>) {
     super(targetNumber, targetType, effectId);
-    if (!Array.isArray(parameters) || parameters.length !== 6 || parameters[4] !== 0 || parameters[5] !== 1) {
+    if (!Array.isArray(parameters) || parameters.length !== 6 || parameters[4] !== 0 || parameters[5] !== 1
+      || (parameters[2] !== 1 && parameters[2] !== 2)) {
       this.parameterError = true;
     } else {
-      this.power = Math.round(parameters[3]);
+      this.basePower = Math.round(parameters[3]);
+      this.bonusPower = Math.round(parameters[1]);
+      this.equipmentGumiId = parameters[0];
     }
   }
 
@@ -24,7 +29,18 @@ export class AbilityDamagePhysicalIncreasedBreakEffect extends SkillEffect {
   }
 
   protected wordEffectImpl(skill: Skill): string {
-    return '';
+    const elements = skill.wordElementInflict();
+    const attackType = skill.wordAttackAndDamageForPhysicalDamages();
+    skill.physique = true;
+    const target = this.wordTarget();
+
+    //+ (this.isEquipmentCategoryFeminine(equipmentGumiId) ? 'une ' : 'un ')
+    //       + this.getEquipmentCategoryTypeWithLink(equipmentGumiId)
+
+    const breakText = `avec un bonus BREAK de ${this.bonusPower}% si l'unité porte un katana`;
+    return `${attackType} ${elements} de puissance ${this.basePower}% ${target} ${breakText}`;
+    //     expect(s).toEqual('Dégâts physiques neutres de puissance 100% à un adversaire avec un bonus BREAK de 400% si l\'unité porte un <a href="ffexvius_objects.php?categid=28">katana</a>');
+
   }
 
 }
