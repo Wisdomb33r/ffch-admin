e6de3cc303a911b90f46fb24146e0458197e3212
Sylvie Stucki <Sylvie@users.noreply.github.com>
2020-10-04 18:23:39 +0200

    #335: Implemented TueurMapper::toDataBaseRepresentation()
    ... with the tests.

--- src/app/ffbe/mappers/tueurs-mapper.spec.ts
+++ src/app/ffbe/mappers/tueurs-mapper.spec.ts
@@ -45,4 +45,26 @@ describe('TueurMapper', () => {
     // THEN
     expect(result).toEqual('');
   });
+
+  it('should map Tueurs with non-null values to string correctly', () => {
+    // GIVEN
+    const tueurs = new Tueurs(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120);
+
+    // WHEN
+    const result = TueursMapper.toDataBaseRepresentation(tueurs);
+
+    // THEN
+    expect(result).toEqual('30,10,20,40,70,80,50,90,60,110,100,120');
+  });
+
+  it('should map Tueurs with mixed values to string correctly', () => {
+    // GIVEN
+    const tueurs = new Tueurs(0, null, 0, null, 10, 50, 0, null, 0, null, 0, 0);
+
+    // WHEN
+    const result = TueursMapper.toDataBaseRepresentation(tueurs);
+
+    // THEN
+    expect(result).toEqual('0,0,0,0,0,0,10,0,50,0,0,0');
+  });
 });
--- src/app/ffbe/mappers/tueurs-mapper.ts
+++ src/app/ffbe/mappers/tueurs-mapper.ts
@@ -1,11 +1,65 @@
 import {Tueurs} from '../model/tueurs.model';
+import {FfbeUtils} from '../utils/ffbe-utils';
+import {FFBE_MONSTER_TYPES} from '../ffbe.constants';
 
 export class TueursMapper {
   public static toDataBaseRepresentation(tueurs: Tueurs): string {
-    return '';
+    if (FfbeUtils.isNullOrUndefined(tueurs)) {
+      return '';
+    }
+
+    const values = Array<number>(FFBE_MONSTER_TYPES.length).fill(0);
+
+    if (TueursMapper.isValidValue(tueurs.aquatiques)) {
+      values[0] = tueurs.aquatiques;
+    }
+    if (TueursMapper.isValidValue(tueurs.betes)) {
+      values[1] = tueurs.betes;
+    }
+    if (TueursMapper.isValidValue(tueurs.oiseaux)) {
+      values[2] = tueurs.oiseaux;
+    }
+    if (TueursMapper.isValidValue(tueurs.demons)) {
+      values[3] = tueurs.demons;
+    }
+    if (TueursMapper.isValidValue(tueurs.dragons)) {
+      values[4] = tueurs.dragons;
+    }
+    if (TueursMapper.isValidValue(tueurs.esprits)) {
+      values[5] = tueurs.esprits;
+    }
+    if (TueursMapper.isValidValue(tueurs.humains)) {
+      values[6] = tueurs.humains;
+    }
+    if (TueursMapper.isValidValue(tueurs.insectes)) {
+      values[7] = tueurs.insectes;
+    }
+    if (TueursMapper.isValidValue(tueurs.machines)) {
+      values[8] = tueurs.machines;
+    }
+    if (TueursMapper.isValidValue(tueurs.plantes)) {
+      values[9] = tueurs.plantes;
+    }
+    if (TueursMapper.isValidValue(tueurs.pierres)) {
+      values[10] = tueurs.pierres;
+    }
+    if (TueursMapper.isValidValue(tueurs.mortsVivants)) {
+      values[11] = tueurs.mortsVivants;
+    }
+
+    if (values.every(value => FfbeUtils.isNullOrUndefined(value) || value === 0)) {
+      return '';
+    }
+
+    return values.join(',');
+  }
+
+  private static isValidValue(value: number): boolean {
+    return !FfbeUtils.isNullOrUndefined(value) && value !== 0;
   }
 
   public static fromDataBaseRepresentation(dbEntry: string): Tueurs {
     return new Tueurs();
   }
+
 }
