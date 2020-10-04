866eecc8a5450f1c23bf647a498480109641a059
Sylvie <Sylvie@users.noreply.github.com>
2020-10-04 12:04:55 +0200

    #335: Added tests for mapping null Tueurs
    -

--- src/app/ffbe/mappers/tueurs-mapper.spec.ts
+++ src/app/ffbe/mappers/tueurs-mapper.spec.ts
@@ -1,9 +1,43 @@
 import {TueursMapper} from './tueurs-mapper';
+import {Tueurs} from '../model/tueurs.model';
 
 describe('TueurMapper', () => {
-  it('should map null Tueur to empty string', () => {
+  it('should map null Tueurs to empty string', () => {
     // GIVEN
     const tueurs = null;
+
+    // WHEN
+    const result = TueursMapper.toDataBaseRepresentation(tueurs);
+
+    // THEN
+    expect(result).toEqual('');
+  });
+
+  it('should map empty Tueurs to empty string', () => {
+    // GIVEN
+    const tueurs = new Tueurs();
+
+    // WHEN
+    const result = TueursMapper.toDataBaseRepresentation(tueurs);
+
+    // THEN
+    expect(result).toEqual('');
+  });
+
+  it('should map Tueurs with only zero values to empty string', () => {
+    // GIVEN
+    const tueurs = new Tueurs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+    // WHEN
+    const result = TueursMapper.toDataBaseRepresentation(tueurs);
+
+    // THEN
+    expect(result).toEqual('');
+  });
+
+  it('should map Tueurs with null and zero values to empty string', () => {
+    // GIVEN
+    const tueurs = new Tueurs(0, null, 0, null, 0, 0, 0, null, 0, null, 0, 0);
 
     // WHEN
     const result = TueursMapper.toDataBaseRepresentation(tueurs);
