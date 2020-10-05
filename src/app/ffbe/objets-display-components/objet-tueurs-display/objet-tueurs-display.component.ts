1b50b1d42da23760d5f453bd7ca51d2498a72e23
Sylvie <Sylvie@users.noreply.github.com>
2020-10-05 22:13:21 +0200

    #335: Added title to the ObjetTueursDisplay Component
    The content is still the placeholder.

--- src/app/ffbe/objets-display-components/objet-tueurs-display/objet-tueurs-display.component.ts
+++ src/app/ffbe/objets-display-components/objet-tueurs-display/objet-tueurs-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-objet-tueurs-display',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjetTueursDisplayComponent implements OnInit {
 
-  constructor() { }
+  @Input() titre: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
