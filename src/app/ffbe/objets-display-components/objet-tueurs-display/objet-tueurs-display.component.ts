3587b5d3039c35855520a8ff452cf1b87e6cee19
Sylvie <Sylvie@users.noreply.github.com>
2020-10-06 07:15:47 +0200

    #335: Injected the Tueur into the ObjetTueursDisplayComponent
    The displayed content is still the placeholder.

--- src/app/ffbe/objets-display-components/objet-tueurs-display/objet-tueurs-display.component.ts
+++ src/app/ffbe/objets-display-components/objet-tueurs-display/objet-tueurs-display.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Tueurs} from '../../model/tueurs.model';
 
 @Component({
   selector: 'app-objet-tueurs-display',
@@ -8,6 +9,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ObjetTueursDisplayComponent implements OnInit {
 
   @Input() titre: string;
+  @Input() tueurs: Tueurs;
 
   constructor() {
   }
--- src/app/ffbe/objets-display-components/objet-tueurs-duo-display/objet-tueurs-duo-display.component.ts
+++ src/app/ffbe/objets-display-components/objet-tueurs-duo-display/objet-tueurs-duo-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Objet} from '../../model/objet/objet.model';
 
 @Component({
   selector: 'app-objet-tueurs-duo-display',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjetTueursDuoDisplayComponent implements OnInit {
 
+  @Input() objet: Objet;
   public displayed = false;
 
   constructor() { }
