0a962de50f6a8d10e96176f4d94356e72c6317dc
Sylvie <Sylvie@users.noreply.github.com>
2021-03-29 06:55:14 +0200

    #410: Implemented the test for storing a string
     The test looks better now.

--- src/app/ffbe/model/storable-form-control.spec.ts
+++ src/app/ffbe/model/storable-form-control.spec.ts
@@ -2,11 +2,10 @@ import {StorableFormControl} from './storable-form-control';
 import {FormControl, Validators} from '@angular/forms';
 
 describe('StorableFormControl', () => {
-  let store;
   let mockLocalStorage;
 
   beforeEach(() => {
-    store = new Map();
+    let store = {};
     mockLocalStorage = {
       getItem: (key: string): string => {
         return key in store ? store[key] : null;
@@ -18,37 +17,36 @@ describe('StorableFormControl', () => {
         delete store[key];
       },
       clear: () => {
-        store.clear();
+        store = {};
       }
     };
+
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
   });
 
   it('should store string value correctly', () => {
     // GIVEN
     const storableFormControl = new StorableFormControl(
-      new FormControl('Hello world!', Validators.required),
-      'local-storage-label');
-    //expect(mockLocalStorage.getItem).toHaveBeenCalledTimes(0);
-
-    spyOn(localStorage, 'setItem')
-      .and.callFake(mockLocalStorage.setItem);
-    spyOn(localStorage, 'getItem').and.throwError('Unexpected call to getItem()');
+      new FormControl('A hello world message to be stored!', Validators.required),
+      'label-for-storing-string');
 
     // WHEN
     storableFormControl.store();
 
     // THEN
-    //expect(store.size).toEqual(1);
-    expect(store['local-storage-label']).toEqual('Hello world!');
+    expect(localStorage.getItem).toHaveBeenCalledTimes(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('label-for-storing-string',
+      'A hello world message to be stored!');
+    expect(localStorage.removeItem).toHaveBeenCalledTimes(0);
+    expect(localStorage.clear).toHaveBeenCalledTimes(0);
   });
 
-
-  /*  spyOn(localStorage, 'getItem')
-    .and.callFake(mockLocalStorage.getItem);
-  spyOn(localStorage, 'setItem')
-    .and.callFake(mockLocalStorage.setItem);
-  spyOn(localStorage, 'removeItem')
-    .and.callFake(mockLocalStorage.removeItem);
-  spyOn(localStorage, 'clear')
-    .and.callFake(mockLocalStorage.clear); */
 });
