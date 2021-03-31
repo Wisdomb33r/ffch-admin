9bad4ab2860663b856deef9ee81075d4a05de710
Wisdomb33r <info@final-fantasy.ch>
2021-03-31 18:32:30 +0200

    Fixed wrong parameter crash for death infliction parsing (#409).

--- src/app/ffbe/model/effects/abilities/ability-death-infliction-effect.model.spec.ts
+++ src/app/ffbe/model/effects/abilities/ability-death-infliction-effect.model.spec.ts
@@ -1,4 +1,5 @@
-import {AbilityEffectParserFactory} from './ability-effect-parser.factory';
+import {AbilityEffectParserFactory} from '../../../mappers/effects/abilities/ability-effect-parser.factory';
+import {AbilitySkillEffectFactory} from '../ability-skill-effect.factory';
 
 describe('AbilityDeathInflictionParser', () => {
 
@@ -6,7 +7,7 @@ describe('AbilityDeathInflictionParser', () => {
     // GIVEN
     const effect = JSON.parse('[0, 3, 35, [100,  1,  1]]');
     // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
     // THEN
     expect(s).toEqual('Inflige Mort (100%) au lanceur (ignore la rés. à Mort)');
   });
@@ -15,7 +16,7 @@ describe('AbilityDeathInflictionParser', () => {
     // GIVEN
     const effect = JSON.parse('[2, 1, 35, [30]]');
     // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
     // THEN
     expect(s).toEqual('Inflige Mort (30%) aux adversaires');
   });
@@ -24,7 +25,7 @@ describe('AbilityDeathInflictionParser', () => {
     // GIVEN
     const effect = JSON.parse('[3, 1, 35, [30, 1, 0]]');
     // WHEN
-    const s = AbilityEffectParserFactory.getParser(effect[0], effect[1], effect[2]).parse(effect, null);
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
     // THEN
     expect(s).toEqual('Inflige Mort (30%) à un adversaire au hasard');
   });
