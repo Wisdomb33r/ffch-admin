59eb11ab0c9fba9fe89ee5bcc014e931aa928b3c
Sylvie <Sylvie@users.noreply.github.com>
2021-04-10 11:23:46 +0200

    The class doesn't do much yet.

--- src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
@@ -1,5 +1,32 @@
 import {EffectParser} from '../../../mappers/effects/effect-parser';
 import {Skill} from '../../skill.model';
+import {SkillEffect} from '../skill-effect.model';
+import {TargetNumberEnum} from '../target-number.enum';
+import {TargetTypeEnum} from '../target-type.enum';
+
+export class AbilityMagIncreaseNextActionEffect extends SkillEffect {
+
+  constructor(protected targetNumber: TargetNumberEnum,
+              protected targetType: TargetTypeEnum,
+              protected effectId: number,
+              protected parameters: Array<any>) {
+    super(targetNumber, targetType, effectId);
+    if (!Array.isArray(parameters) || parameters.length < 3
+      || targetNumber !== 0 || targetType !== 3 || parameters[2] !== 0) {
+      this.parameterError = true;
+    } else {
+
+    }
+  }
+
+  protected wordEffectImpl(skill: Skill): string {
+    return '';
+  }
+
+  protected get effectName(): string {
+    return 'AbilityMagIncreaseNextActionEffect';
+  }
+}
 
 export class AbilityMagIncreaseNextAction extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
