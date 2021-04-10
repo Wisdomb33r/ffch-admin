6580d2a7f3efb85e72efb42fad7e1422cf2db9fd
Sylvie <Sylvie@users.noreply.github.com>
2021-04-10 21:59:46 +0200

    #336: Added draft of PassiveCounterAttackEffect
     It doesn't do much yet.

--- src/app/ffbe/model/effects/passives/passive-counter-attack-effect.model.ts
+++ src/app/ffbe/model/effects/passives/passive-counter-attack-effect.model.ts
@@ -1,5 +1,30 @@
 import {EffectParser} from '../../../mappers/effects/effect-parser';
 import {Skill} from '../../skill.model';
+import {SkillEffect} from '../skill-effect.model';
+import {TargetNumberEnum} from '../target-number.enum';
+import {TargetTypeEnum} from '../target-type.enum';
+
+export class PassiveCounterAttackEffect extends SkillEffect {
+
+  constructor(protected targetNumber: TargetNumberEnum,
+              protected targetType: TargetTypeEnum,
+              protected effectId: number,
+              protected parameters: Array<any>) {
+    super(targetNumber, targetType, effectId);
+    if (!Array.isArray(parameters) || parameters.length < 3) {
+      this.parameterError = true;
+    } else {
+    }
+  }
+
+  protected wordEffectImpl(skill: Skill): string {
+    return '';
+  }
+
+  protected get effectName(): string {
+    return 'PassiveCounterAttackEffect';
+  }
+}
 
 export class PassiveCounterAttackParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
