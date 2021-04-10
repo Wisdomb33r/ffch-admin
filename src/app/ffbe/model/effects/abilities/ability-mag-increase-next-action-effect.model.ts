95b854160b60134e9fc2aa72bb4854ec8b826689
Sylvie <Sylvie@users.noreply.github.com>
2021-04-10 20:02:05 +0200

    It replaces part of the parameter error handling.

--- src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
@@ -1,4 +1,3 @@
-import {EffectParser} from '../../../mappers/effects/effect-parser';
 import {Skill} from '../../skill.model';
 import {SkillEffect} from '../skill-effect.model';
 import {TargetNumberEnum} from '../target-number.enum';
@@ -15,9 +14,12 @@ export class AbilityMagIncreaseNextActionEffect extends SkillEffect {
               protected parameters: Array<any>) {
     super(targetNumber, targetType, effectId);
     if (!Array.isArray(parameters) || parameters.length < 3
-      || targetNumber !== 0 || targetType !== 3 || parameters[2] !== 0) {
+      || targetNumber !== 0 || targetType !== 3) {
       this.parameterError = true;
     } else {
+      if (parameters[2] !== 0 && parameters[2] !== 50) {
+        this.parameterWarning = true;
+      }
       this.magIncrease = parameters[0];
       this.magIncreaseMax = parameters[1];
     }
