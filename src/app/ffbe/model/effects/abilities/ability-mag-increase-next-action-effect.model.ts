d2c6a490ec397997465efd0afde890db374c568c
Sylvie <Sylvie@users.noreply.github.com>
2021-04-10 11:32:08 +0200

    It was no longer used.

--- src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/ability-mag-increase-next-action-effect.model.ts
@@ -33,18 +33,3 @@ export class AbilityMagIncreaseNextActionEffect extends SkillEffect {
     return 'AbilityMagIncreaseNextActionEffect';
   }
 }
-
-export class AbilityMagIncreaseNextAction extends EffectParser {
-  public parse(effect: Array<any>, skill: Skill): string {
-    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 3
-      || effect[0] !== 0 || effect[1] !== 3 || effect[3][2] !== 0) {
-      return 'Effet AbilityMagIncreaseNextAction inconnu: Mauvaise liste de paramètres';
-    }
-
-    const magIncrease = effect[3][0];
-    const magIncreaseMax = effect[3][1];
-    const increaseMaxText = magIncreaseMax > magIncrease ? ` (cumulable, +${magIncreaseMax}% max)` : '';
-
-    return `+${magIncrease}% MAG pour la prochaine action du lanceur${increaseMaxText}`;
-  }
-}
