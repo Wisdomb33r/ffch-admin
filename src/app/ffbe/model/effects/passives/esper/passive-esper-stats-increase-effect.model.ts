98ce6ec645076d8c078a94b693746cba11d3445f
Sylvie Stucki <Sylvie@users.noreply.github.com>
2021-05-15 17:52:40 +0200

    #333: Removed PassiveEsperStatsIncreaseParser
    It was no longer used.

--- src/app/ffbe/model/effects/passives/esper/passive-esper-stats-increase-effect.model.ts
+++ src/app/ffbe/model/effects/passives/esper/passive-esper-stats-increase-effect.model.ts
@@ -1,4 +1,3 @@
-import {EffectParser} from '../../../../mappers/effects/effect-parser';
 import {Skill} from '../../../skill.model';
 import {FFBE_ESPERS} from '../../../../ffbe.constants';
 import {SkillEffect} from '../../skill-effect.model';
@@ -48,34 +47,5 @@ export class PassiveEsperStatsIncreaseEffect extends SkillEffect {
 
   protected get effectName(): string {
     return 'PassiveEsperStatsIncreaseEffect';
-  }
-}
-
-export class PassiveEsperStatsIncreaseParser extends EffectParser {
-  public parse(effect: Array<any>, skill: Skill): string {
-    if (effect.length < 4 || !Array.isArray(effect[3]) || effect[3].length < 6) {
-      return 'Effet PassiveEsperStatsIncreaseParser inconnu: Mauvaise liste de paramètres';
-    }
-
-    const esperId = effect[3][6];
-    const esper = FFBE_ESPERS.find(e => e.gumiId === esperId);
-
-    const increases = [
-      {name: 'PV', value: effect[3][4]},
-      {name: 'PM', value: effect[3][5]},
-      {name: 'ATT', value: effect[3][0]},
-      {name: 'DÉF', value: effect[3][1]},
-      {name: 'MAG', value: effect[3][2]},
-      {name: 'PSY', value: effect[3][3]},
-    ];
-    return this.wordEffectJoiningIdenticalValues(increases) + ' obtenues par la chimère'
-      + (esperId > 0 ? ' ' + SkillEffect.getEsperLink(esper) : '');
-  }
-
-  protected wordEffectForIdenticalValues(currentValue, accumulatedStats: Array<string>): string {
-    if (accumulatedStats.length === 6) {
-      return '+' + currentValue + '% aux caractéristiques';
-    }
-    return '+' + currentValue + '% ' + accumulatedStats.join('/');
   }
 }
