b727b1a147be408865f7ec38649049aa859407a8
Sylvie Stucki <Sylvie@users.noreply.github.com>
2021-05-24 11:16:10 +0200

    #444: Added skeleton of PassiveDebuffsResistanceEffect
     It doesn't do much yet.

--- src/app/ffbe/model/effects/passives/passive-debuffs-resistance-effect.model.ts
+++ src/app/ffbe/model/effects/passives/passive-debuffs-resistance-effect.model.ts
@@ -1,6 +1,32 @@
 import {EffectParser} from '../../../mappers/effects/effect-parser';
 import {Skill} from '../../skill.model';
 import {HTML_LINE_RETURN} from '../../../mappers/effects/skill-effects.mapper';
+import {SkillEffect} from '../skill-effect.model';
+import {TargetNumberEnum} from '../target-number.enum';
+import {TargetTypeEnum} from '../target-type.enum';
+
+export class PassiveDebuffsResistanceEffect extends SkillEffect {
+
+  constructor(protected targetNumber: TargetNumberEnum,
+              protected targetType: TargetTypeEnum,
+              protected effectId: number,
+              protected parameters: Array<any>) {
+    super(targetNumber, targetType, effectId);
+    if (!Array.isArray(parameters) || parameters.length < 1) {
+      this.parameterError = true;
+    } else {
+
+    }
+  }
+
+  protected wordEffectImpl(skill: Skill): string {
+    return '';
+  }
+
+  protected get effectName(): string {
+    return 'PassiveDebuffsResistanceEffect';
+  }
+}
 
 export class PassiveDebuffsResistanceParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
