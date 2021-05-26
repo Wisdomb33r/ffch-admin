fcae2e074a5ae3490d9bbb46552391baddbac88e
Sylvie <Sylvie@users.noreply.github.com>
2021-05-26 21:49:17 +0200

    #443: Added skeleton of PassiveEquipmentCategoryUnlockEffect
     -

--- src/app/ffbe/model/effects/passives/passive-equipment-category-unlock-effect.model.ts
+++ src/app/ffbe/model/effects/passives/passive-equipment-category-unlock-effect.model.ts
@@ -1,5 +1,30 @@
 import {EffectParser} from '../../../mappers/effects/effect-parser';
 import {Skill} from '../../skill.model';
+import {SkillEffect} from '../skill-effect.model';
+import {TargetNumberEnum} from '../target-number.enum';
+import {TargetTypeEnum} from '../target-type.enum';
+
+export class PassiveEquipmentCategoryUnlockEffect extends SkillEffect {
+
+  constructor(protected targetNumber: TargetNumberEnum,
+              protected targetType: TargetTypeEnum,
+              protected effectId: number,
+              protected parameters: Array<any>) {
+    super(targetNumber, targetType, effectId);
+    if (!Array.isArray(parameters) || parameters.length < 1) {
+      this.parameterError = true;
+    }
+  }
+
+  protected wordEffectImpl(skill: Skill): string {
+    return '';
+  }
+
+  protected get effectName(): string {
+    return 'export class PassiveEquipmentCategoryUnlockEffect extends SkillEffect {\n';
+  }
+
+}
 
 export class PassiveEquipmentCategoryUnlockParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
