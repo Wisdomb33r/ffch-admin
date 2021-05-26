f06c6d87cec8582c8624554b30f4d5453f67cd15
Sylvie <Sylvie@users.noreply.github.com>
2021-05-26 22:04:30 +0200

    #443: Added skeleton of PassiveEquipmentCategoryStatsIncreaseEffect
     -

--- src/app/ffbe/model/effects/passives/stats/passive-equipment-category-stats-increase-effect.model.ts
+++ src/app/ffbe/model/effects/passives/stats/passive-equipment-category-stats-increase-effect.model.ts
@@ -1,6 +1,33 @@
 import {EffectParser} from '../../../../mappers/effects/effect-parser';
 import {Skill} from '../../../skill.model';
 import {FfbeUtils} from '../../../../utils/ffbe-utils';
+import {SkillEffect} from '../../skill-effect.model';
+import {TargetNumberEnum} from '../../target-number.enum';
+import {TargetTypeEnum} from '../../target-type.enum';
+
+export class PassiveEquipmentCategoryStatsIncreaseEffect extends SkillEffect {
+
+  constructor(protected targetNumber: TargetNumberEnum,
+              protected targetType: TargetTypeEnum,
+              protected effectId: number,
+              protected parameters: Array<any>) {
+    super(targetNumber, targetType, effectId);
+    if (!Array.isArray(parameters) || parameters.length < 6) {
+      this.parameterError = true;
+    } else {
+    }
+  }
+
+  protected wordEffectImpl(skill: Skill): string {
+    return '';
+  }
+
+  protected get effectName(): string {
+    return 'PassiveEquipmentCategoryStatsIncreaseEffect';
+  }
+
+}
+
 
 export class PassiveEquipmentCategoryStatsIncreaseParser extends EffectParser {
   public parse(effect: Array<any>, skill: Skill): string {
