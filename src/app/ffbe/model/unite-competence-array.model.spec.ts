cd9e59927b22a17cdc0ac3bc35ff4523a4d977a8
Sylvie <Sylvie@users.noreply.github.com>
2021-06-25 22:42:46 +0200

    #300: Added the function shuffleArray for testing
     Shuffling the UniteCompetenceArray representing the database version
     will ensure the comparison method is correct.

--- src/app/ffbe/model/unite-competence-array.model.spec.ts
+++ src/app/ffbe/model/unite-competence-array.model.spec.ts
@@ -6,7 +6,7 @@ describe('UniteCompetenceArray', () => {
   it('should compare identical UniteCompetenceArray correctly', () => {
     // GIVEN
     const dmUniteCompetences = createUniteCompetenceArray();
-    const dbUniteCompetences= createUniteCompetenceArray();
+    const dbUniteCompetences = createUniteCompetenceArray();
 
     // WHEN
     const result = dmUniteCompetences.compareWithDatabase(dbUniteCompetences);
@@ -17,41 +17,67 @@ describe('UniteCompetenceArray', () => {
     expect(dbUniteCompetences.length).toEqual(4);
     expect(dmUniteCompetences.every(uniteCompetence => uniteCompetence.status === UniteCompetenceStatus.Correct)).toBeTrue();
   });
+
+  it('should compare shuffled identical UniteCompetenceArray correctly', () => {
+    // GIVEN
+    const dmUniteCompetences = createUniteCompetenceArray();
+    const dbUniteCompetences = createUniteCompetenceArray();
+    shuffleArray(dbUniteCompetences);
+
+    // WHEN
+    const result = dmUniteCompetences.compareWithDatabase(dbUniteCompetences);
+
+    // THEN
+    expect(Array.isArray(result)).toBeTrue();
+    expect(result.length).toEqual(0);
+    expect(dbUniteCompetences.length).toEqual(4);
+    expect(dmUniteCompetences.every(uniteCompetence => uniteCompetence.status === UniteCompetenceStatus.Correct)).toBeTrue();
+  });
+
+
+  function createUniteCompetenceArray(): UniteCompetenceArray {
+    return new UniteCompetenceArray(
+      new UniteCompetence(createCompetence(123, 'Coup de pied', 'Kick'), 1),
+      new UniteCompetence(createCompetence(456, 'PV +10%', 'HP +10%'), 80),
+      new UniteCompetence(createCompetence(789, 'Concombre masqué', 'Masked Cucumber'), 101),
+      new UniteCompetence(createCompetence(321, 'Gros boum', 'Big boom'), 120),);
+  }
+
+  function createCompetence(gumi_id: number, nom: string, nom_en: string): Competence {
+    return new Competence(gumi_id,
+      0,
+      0,
+      false,
+      false,
+      false,
+      false,
+      false,
+      0,
+      nom,
+      nom_en,
+      '',
+      '',
+      '',
+      '',
+      '',
+      0,
+      0,
+      0,
+      0,
+      0,
+      '',
+      '',
+      '',
+      false);
+  }
+
+  function shuffleArray(array: Array<any>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
 });
-
-function createUniteCompetenceArray(): UniteCompetenceArray {
-  return new UniteCompetenceArray(
-    new UniteCompetence(createCompetence(123, 'Coup de pied', 'Kick'), 1),
-    new UniteCompetence(createCompetence(456, 'PV +10%', 'HP +10%'), 80),
-    new UniteCompetence(createCompetence(789, 'Concombre masqué', 'Masked Cucumber'), 101),
-    new UniteCompetence(createCompetence(321, 'Gros boum', 'Big boom'), 120),);
-}
-
-function createCompetence(gumi_id: number, nom: string, nom_en: string): Competence {
-  return new Competence(gumi_id,
-    0,
-    0,
-    false,
-    false,
-    false,
-    false,
-    false,
-    0,
-    nom,
-    nom_en,
-    '',
-    '',
-    '',
-    '',
-    '',
-    0,
-    0,
-    0,
-    0,
-    0,
-    '',
-    '',
-    '',
-    false);
-}
-
