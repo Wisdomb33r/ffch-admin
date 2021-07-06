18d488ca8149e97418be37cd5ee6acec3973cd3b
Sylvie <Sylvie@users.noreply.github.com>
2021-07-06 06:47:48 +0200

    #483: Changed the morale display to percentage
     ... and added the "morale per ally" gain.

--- src/app/ffbe/model/effects/abilities/ability-morale-jauge-effect.model.spec.ts
+++ src/app/ffbe/model/effects/abilities/ability-morale-jauge-effect.model.spec.ts
@@ -2,12 +2,48 @@ import {AbilitySkillEffectFactory} from '../ability-skill-effect.factory';
 
 describe('AbilityMoraleJaugeEffect', () => {
 
-  it('should parse morale jauge filling', () => {
+  it('should parse morale jauge increase correctly', () => {
     // GIVEN
     const effect = JSON.parse('[0, 3, 1015, [650,  0,  0,  0,  0,  0,  0]]');
     // WHEN
     const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
     // THEN
-    expect(s).toEqual('+650 à la jauge de moral');
+    expect(s).toEqual('+6.5% à la jauge de moral');
+  });
+
+  it('should parse morale jauge decrease correctly', () => {
+    // GIVEN
+    const effect = JSON.parse('[0, 3, 1015, [-300,  0,  0,  0,  0,  0,  0]]');
+    // WHEN
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
+    // THEN
+    expect(s).toEqual('-3% à la jauge de moral');
+  });
+
+  it('should parse morale jauge increase per active ally correctly', () => {
+    // GIVEN
+    const effect = JSON.parse('[0, 3, 1015, [0,  200,  0,  0,  0,  1,  0]]');
+    // WHEN
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
+    // THEN
+    expect(s).toEqual('+2% à la jauge de moral par allié actif');
+  });
+
+  it('should parse morale jauge decrease per active ally correctly', () => {
+    // GIVEN
+    const effect = JSON.parse('[0, 3, 1015, [0,  -50,  0,  0,  0,  1,  0]]');
+    // WHEN
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
+    // THEN
+    expect(s).toEqual('-0.5% à la jauge de moral par allié actif');
+  });
+
+  it('should parse combined morale jauge changes correctly', () => {
+    // GIVEN
+    const effect = JSON.parse('[0, 3, 1015, [-350,  400,  0,  0,  0,  0,  0]]');
+    // WHEN
+    const s = AbilitySkillEffectFactory.getSkillEffect(effect).wordEffect(undefined);
+    // THEN
+    expect(s).toEqual('-3.5% à la jauge de moral<br />+4% à la jauge de moral par allié actif');
   });
 });
--- src/app/ffbe/model/effects/abilities/ability-morale-jauge-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/ability-morale-jauge-effect.model.ts
@@ -2,10 +2,12 @@ import {SkillEffect} from '../skill-effect.model';
 import {Skill} from '../../skill.model';
 import {TargetNumberEnum} from '../target-number.enum';
 import {TargetTypeEnum} from '../target-type.enum';
+import {HTML_LINE_RETURN} from '../../../mappers/effects/skill-effects.mapper';
 
 export class AbilityMoraleJaugeEffect extends SkillEffect {
 
   private moraleIncrease: number;
+  private moraleIncreasePerAlly: number;
 
   constructor(protected targetNumber: TargetNumberEnum,
               protected targetType: TargetTypeEnum,
@@ -15,15 +17,29 @@ export class AbilityMoraleJaugeEffect extends SkillEffect {
     if (!Array.isArray(parameters) || parameters.length < 1) {
       this.parameterError = true;
     } else {
-      if (parameters.length !== 7 || parameters.splice(1).every(value => value === 0)) {
+      if (parameters.length !== 7 || parameters[2] !== 0 || parameters[3] !== 0 ||
+        parameters[4] !== 0 || (parameters[5] !== 0 && parameters[5] !== 1) || parameters[6] !== 0) {
         this.parameterWarning = true;
       }
-      this.moraleIncrease = parameters[0];
+      this.moraleIncrease = parameters[0] / 100;
+      if (parameters.length > 1) {
+        this.moraleIncreasePerAlly = parameters[1] ? parameters[1] / 100 : 0;
+      }
     }
   }
 
   protected wordEffectImpl(skill: Skill): string {
-    return `+${this.moraleIncrease} à la jauge de moral`;
+    const moraleSign = this.moraleIncrease >= 0 ? '+' : '';
+    const moraleText = `${moraleSign}${this.moraleIncrease}% à la jauge de moral`;
+    const moralePerAllySign = this.moraleIncreasePerAlly >= 0 ? '+' : '';
+    const moralePerAllyText = `${moralePerAllySign}${this.moraleIncreasePerAlly}% à la jauge de moral par allié actif`;
+    if (this.moraleIncrease !== 0 && this.moraleIncreasePerAlly !== 0) {
+      return `${moraleText}${HTML_LINE_RETURN}${moralePerAllyText}`;
+    } else if (this.moraleIncreasePerAlly !== 0) {
+      return `${moralePerAllyText}`;
+    } else {
+      return `${moraleText}`;
+    }
   }
 
   protected get effectName(): string {
