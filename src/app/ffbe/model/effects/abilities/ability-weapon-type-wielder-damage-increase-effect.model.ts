0419bac3a9762588b8752cc3d667b9b21c733c41
Wisdomb33r <info@final-fantasy.ch>
2021-07-07 13:34:41 +0200

    Fixing a wrong parameter error for a warning in weapon type wielder damage increase parsing (#482).

--- src/app/ffbe/model/effects/abilities/ability-weapon-type-wielder-damage-increase-effect.model.ts
+++ src/app/ffbe/model/effects/abilities/ability-weapon-type-wielder-damage-increase-effect.model.ts
@@ -16,10 +16,12 @@ export class AbilityWeaponTypeWielderDamageIncreaseEffect extends SkillEffect {
               protected effectId: number,
               protected parameters: Array<any>) {
     super(targetNumber, targetType, effectId);
-    if (!Array.isArray(parameters) || parameters.length < 6 || parameters[3] !== 1 || parameters[5] !== 1
-      || parameters[1] !== parameters[2]) {
+    if (!Array.isArray(parameters) || parameters.length < 6 || parameters[1] !== parameters[2]) {
       this.parameterError = true;
     } else {
+      if (parameters[3] !== 1 || [0, 1].indexOf(parameters[5]) === -1) {
+        this.parameterWarning = true;
+      }
       this.weaponType = parameters[0];
       this.increase = parameters[1];
       this.turns = parameters[4];
@@ -27,7 +29,7 @@ export class AbilityWeaponTypeWielderDamageIncreaseEffect extends SkillEffect {
   }
 
   protected wordEffectImpl(skill: Skill) {
-    const categorie = FFBE_CATEGORIES_OBJETS.find(categorie => categorie.gumiId === this.weaponType);
+    const categorie = FFBE_CATEGORIES_OBJETS.find(cat => cat.gumiId === this.weaponType);
     const weaponTypeText = categorie ? categorie.name : 'UNKNOWN WEAPON TYPE';
     const target = this.wordTarget(TargetPrepositionEnum.None);
     const turnsText = `pour ${this.turns} tour${this.turns > 1 ? 's' : ''}`;
