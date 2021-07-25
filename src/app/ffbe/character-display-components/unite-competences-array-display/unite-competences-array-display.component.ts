9e66be084750655bde6405a9c9992446ea749494
Sylvie <Sylvie@users.noreply.github.com>
2021-07-25 21:21:23 +0200

    #300: Added button to send a missing UniteCompetence to the backend
     The button just logs a message yet.

--- src/app/ffbe/character-display-components/unite-competences-array-display/unite-competences-array-display.component.ts
+++ src/app/ffbe/character-display-components/unite-competences-array-display/unite-competences-array-display.component.ts
@@ -1,7 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UniteCompetence, UniteCompetenceStatus} from '../../model/unite-competence.model';
+import {Competence} from '../../model/competence.model';
 import {FfbeUtils} from '../../utils/ffbe-utils';
 import {FfchClientService} from '../../services/ffch-client.service';
+
 
 @Component({
   selector: 'app-unite-competences-array-display',
@@ -12,6 +14,7 @@ export class UniteCompetencesArrayDisplayComponent implements OnInit {
 
   @Input() uniteCompetences: Array<UniteCompetence>;
   @Input() titre: string;
+  @Input() allowPosting = true;
   @Input() showLevelMismatch = true;
 
   constructor(private ffchClientService: FfchClientService) {
@@ -20,12 +23,24 @@ export class UniteCompetencesArrayDisplayComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public hasLevelMismatch(uniteCompetence: UniteCompetence) {
+  public hasLevelMismatch(uniteCompetence: UniteCompetence): boolean {
     return this.showLevelMismatch && !FfbeUtils.isNullOrUndefined(UniteCompetence) && uniteCompetence.status === UniteCompetenceStatus.LevelMismatch;
   }
 
-  public isNotFoundInCounterPart(uniteCompetence: UniteCompetence) {
+  public isNotFoundInCounterPart(uniteCompetence: UniteCompetence): boolean {
     return !FfbeUtils.isNullOrUndefined(UniteCompetence) && uniteCompetence.status === UniteCompetenceStatus.NotFoundInCounterPart;
+  }
+
+  public isMissingInFfchDb(uniteCompetence: UniteCompetence): boolean {
+    return this.allowPosting && !FfbeUtils.isNullOrUndefined(UniteCompetence) && uniteCompetence.status === UniteCompetenceStatus.NotFoundInCounterPart;
+  }
+
+  public isCompetencePresentInFfchDb(competence: Competence): boolean {
+    return !FfbeUtils.isNullOrUndefined(competence?.id);
+  }
+
+  public sendUniteCompetenceToFfch(uniteCompetence: UniteCompetence) {
+    console.log('Sent!');
   }
 
   public updateUniteCompetenceInFfch(uniteCompetence: UniteCompetence) {
