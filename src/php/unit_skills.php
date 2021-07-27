8d0136f1d6f984409653d78164733a6dc1493e8d
Sylvie <Sylvie@users.noreply.github.com>
2021-07-27 07:48:55 +0200

    #300: The backend now handles POST requests for UniteCompetences
     The Unite id is parsed from the query string.

--- src/php/unit_skills.php
+++ src/php/unit_skills.php
@@ -6,7 +6,34 @@ require_once "includes/skill_class.php";
 require_once "includes/unit_skill_class.php";
 
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
-  dieWithBadRequest('Not implemented yet');
+  $uniteCompetence = json_decode ( file_get_contents ( 'php://input' ) );
+
+  if (!isset ($uniteCompetence->competence) || !$uniteCompetence->competence) {
+    dieWithBadRequest('Format exception : cannot save without Compétence');
+  }
+
+  if (!isset ($uniteCompetence->competence->id) || !$uniteCompetence->competence->id) {
+    dieWithBadRequest('Format exception : cannot save without Compétence id');
+  }
+
+  if (!isset ($uniteCompetence->niveau) || !$uniteCompetence->niveau) {
+    dieWithBadRequest('Format exception : cannot save without niveau');
+  }
+
+  $post_query_string = $_SERVER ['QUERY_STRING'];
+  if (!isset($post_query_string) || !$post_query_string) {
+    dieWithBadRequest('Format exception: Query string must contain uniteId');
+  }
+
+  parse_str($post_query_string, $parsed_query_string);
+
+  if (!isset ( $parsed_query_string ['uniteId'] )) {
+      dieWithBadRequest('Format exception : cannot save without Unité id');
+  }
+
+  $brex_unit_comp = createAndValidateBrexUnitComp($uniteCompetence, $parsed_query_string ['uniteId']);
+  storeAndRespond($brex_unit_comp, 201);
+
 } else if ($_SERVER ['REQUEST_METHOD'] == 'PUT') {
   $uniteCompetence = json_decode ( file_get_contents ( 'php://input' ) );
   if (isset($uniteCompetence->id) && $uniteCompetence->id) {
@@ -51,6 +78,34 @@ function storeAndRespond($brex_unit_comp, $successCode) {
     http_response_code ( 400 );
     echo json_encode ( $brex_unit_comp->errors );
   }
+}
+
+
+function createAndValidateBrexUnitComp($uniteCompetence, $uniteId)
+{
+  $brex_unit = brex_unit::findByPrimaryId($uniteId);
+  if(!$brex_unit) {
+    dieWithBadRequest('Storage exception: Unite with id ' . $uniteId .  ' not found.');
+  }
+
+  $brex_competence = brex_competence::findByPrimaryId($uniteCompetence->competence->id);
+  if(!$brex_competence) {
+    dieWithBadRequest('Storage exception: Competence with id ' . $uniteId . ' (gumiId=' . $uniteCompetence->competence->gumi_id . ') not found.');
+  }
+
+  $values = array();
+  $values ['niveau'] = $uniteCompetence->niveau;
+
+  $brex_unit_comp = new brex_unit_comp ($values);
+
+  $brex_unit_comp->setrelationunit($brex_unit);
+  $brex_unit_comp->setrelationcompetence($brex_competence);
+
+  if (!$brex_unit_comp->verifyValues()) {
+    dieWithBadRequest(array_merge($brex_unit_comp->errors, (array)'Format exception: Validation of brex_unit_comp failed for competence ' . $brex_competence->gumi_id));
+  }
+
+  return $brex_unit_comp;
 }
 
 ?>
