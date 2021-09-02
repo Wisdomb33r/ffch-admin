2c33661a09a530c620c29cc97af5b0009f55cdbc
Sylvie <Sylvie@users.noreply.github.com>
2021-09-02 21:51:49 +0200

    #373: Refactored target conversion for FieldSkillEffects
     Now the target can be worded for both passive and active underlying SkillEffects.

--- src/app/ffbe/model/effects/abstract-field-skill-effect.model.ts
+++ src/app/ffbe/model/effects/abstract-field-skill-effect.model.ts
@@ -4,23 +4,30 @@ import {TargetTypeEnum} from './target-type.enum';
 import {TargetPrepositionEnum} from './target-preposition.enum';
 
 export abstract class AbstractFieldSkillEffect extends SkillEffect {
+
+  protected convertedTarget: [TargetNumberEnum, TargetTypeEnum];
+
   constructor(protected targetNumber: TargetNumberEnum,
               protected targetType: TargetTypeEnum,
               protected effectId: number,
               protected duration: number) {
     super(targetNumber, targetType, effectId);
+    this.convertedTarget = this.convertTarget();
   }
 
-  protected wordTarget(preposition: TargetPrepositionEnum = TargetPrepositionEnum.A): string {
+  protected wordTarget(preposition: TargetPrepositionEnum = TargetPrepositionEnum.A) {
+    return this.wordSpecificTarget(preposition, this.convertedTarget[0], this.convertedTarget[1]);
+  }
 
+  protected convertTarget(): [TargetNumberEnum, TargetTypeEnum] {
     // TODO: Handle other cases!
     if (this.targetNumber === TargetNumberEnum.Self && this.targetType === TargetTypeEnum.Caster) {
-      return SkillEffect.getTargetAlliesAndEnemiesText(preposition);
+      return [TargetNumberEnum.Multiple, TargetTypeEnum.AllyAndEnemy];
     } else if (this.targetNumber === TargetNumberEnum.Single && this.targetType === TargetTypeEnum.Enemy) {
-      return SkillEffect.getTargetEnemiesText(preposition);
+      return [TargetNumberEnum.Multiple, TargetTypeEnum.Enemy];
     }
 
-    return 'UNKNOWN target';
+    return [0, 0];
   }
 
 }
